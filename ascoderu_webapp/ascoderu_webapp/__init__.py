4adfc51d7c34ac2f5421ef6c341dfda533fb3716
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-07 23:52:45 -0700

    Add boilerplate for internationalization

--- ascoderu_webapp/ascoderu_webapp/__init__.py
+++ ascoderu_webapp/ascoderu_webapp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+babel = Babel(app)
 
 from ascoderu_webapp import views
--- ascoderu_webapp/ascoderu_webapp/views.py
+++ ascoderu_webapp/ascoderu_webapp/views.py
@@ -1,6 +1,14 @@
 from flask import render_template
+from flask import request
 
 from ascoderu_webapp import app
+from ascoderu_webapp import babel
+from config import LANGUAGES
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(LANGUAGES.keys())
 
 
 @app.route('/')
