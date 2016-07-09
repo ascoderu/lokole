ec1996e0177a957693a4076c539ffa73d00f2023
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-09 16:59:19 -0700

    Make form fields localizable

--- ascoderu_webapp/ascoderu_webapp/forms.py
+++ ascoderu_webapp/ascoderu_webapp/forms.py
@@ -1,12 +1,23 @@
+from flask_babel import gettext
 from flask_security import LoginForm as BaseLoginForm
 from flask_security import RegisterForm as BaseRegisterForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
+MESSAGES = {
+    'email_field': gettext('Name or Email'),
+    'email_field_required': gettext('Please enter your name or email.'),
+}
+
+
 class LoginForm(BaseLoginForm):
-    email = StringField('Name or Email', [DataRequired()])
+    email = StringField(
+        label=MESSAGES['email_field'],
+        validators=[DataRequired(MESSAGES['email_field_required'])])
 
 
 class RegisterForm(BaseRegisterForm):
-    email = StringField('Name', [DataRequired()])
+    email = StringField(
+        label=MESSAGES['email_field'],
+        validators=[DataRequired(MESSAGES['email_field_required'])])
