962cd9fb9c3c39d6753626146366ed836cd8c65e
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-09 17:01:19 -0700

    Customize flask_security login and register pages
    Changes include:
    1) Use master page
    2) Apply boostrap styles

--- ascoderu_webapp/ascoderu_webapp/forms.py
+++ ascoderu_webapp/ascoderu_webapp/forms.py
@@ -8,6 +8,8 @@ from wtforms.validators import DataRequired
 MESSAGES = {
     'email_field': gettext('Name or Email'),
     'email_field_required': gettext('Please enter your name or email.'),
+    'name_field': gettext('Name'),
+    'name_field_required': gettext('Please enter your name.'),
 }
 
 
@@ -19,5 +21,5 @@ class LoginForm(BaseLoginForm):
 
 class RegisterForm(BaseRegisterForm):
     email = StringField(
-        label=MESSAGES['email_field'],
-        validators=[DataRequired(MESSAGES['email_field_required'])])
+        label=MESSAGES['name_field'],
+        validators=[DataRequired(MESSAGES['name_field_required'])])
