7446814a45484cef609444f4329c97bdfc436a18
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-10 19:29:17 -0700

    Rename utils.babel to script_commands

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_migrate import MigrateCommand
 from flask_script import Manager
 
 from ascoderu_webapp import app
-from utils.babel import BabelCommand
+from utils.script_commands import BabelCommand
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
