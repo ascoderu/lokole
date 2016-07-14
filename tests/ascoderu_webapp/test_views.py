250aca79f289934cfca78fe30cd5007832744840
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-13 21:59:05 -0700

    Add unit test for login-protection of views

---
+++ tests/ascoderu_webapp/test_views.py
@@ -0,0 +1,14 @@
+from flask import request
+from flask_testing import TestCase
+
+from utils.testing import AppTestMixin
+
+
+class TestLoginRequiredViews(AppTestMixin, TestCase):
+    LOGIN_REQUIRED_ENDPOINTS = ['/email/sent', '/email/inbox', '/email/outbox']
+
+    def test_private_endpoints_disallow_not_logged_in_user(self):
+        with self.app.test_client() as client:
+            for endpoint in self.LOGIN_REQUIRED_ENDPOINTS:
+                client.get(endpoint, follow_redirects=True)
+                self.assertEqual(request.endpoint, 'security.login')
