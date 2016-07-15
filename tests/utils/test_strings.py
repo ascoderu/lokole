9451a463a0f340610f399e32e098428c83639665
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-15 00:36:01 -0700

    Directly import method under test
    This reduces the verbosity of the test code as it reduces the extra
    references to the module-name that don't buy as anything in terms of
    expressiveness.

--- tests/utils/test_strings.py
+++ tests/utils/test_strings.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
-from utils import strings
+from utils.strings import istreq
 
 
 class TestIstreq(TestCase):
     def test_none_not_equal(self):
-        self.assertFalse(strings.istreq('1', None))
-        self.assertFalse(strings.istreq(None, '1'))
-        self.assertTrue(strings.istreq(None, None))
+        self.assertFalse(istreq('1', None))
+        self.assertFalse(istreq(None, '1'))
+        self.assertTrue(istreq(None, None))
 
     def test_non_string_types_not_equal(self):
-        self.assertFalse(strings.istreq(1, 1))
-        self.assertFalse(strings.istreq(1, '1'))
-        self.assertFalse(strings.istreq('1', 1))
+        self.assertFalse(istreq(1, 1))
+        self.assertFalse(istreq(1, '1'))
+        self.assertFalse(istreq('1', 1))
 
     def test_same_strings_are_equal(self):
-        self.assertFalse(strings.istreq('1', 'ß'))
-        self.assertTrue(strings.istreq('1', '1'))
-        self.assertTrue(strings.istreq('ß', 'ß'))
+        self.assertFalse(istreq('1', 'ß'))
+        self.assertTrue(istreq('1', '1'))
+        self.assertTrue(istreq('ß', 'ß'))
 
     def test_same_strings_are_equal_unicode_normalization(self):
-        self.assertFalse(strings.istreq('ê', 'a'))
-        self.assertTrue(strings.istreq('ê', 'ê'))
-        self.assertTrue(strings.istreq('Σίσυφος', 'ΣΊΣΥΦΟΣ'))
+        self.assertFalse(istreq('ê', 'a'))
+        self.assertTrue(istreq('ê', 'ê'))
+        self.assertTrue(istreq('Σίσυφος', 'ΣΊΣΥΦΟΣ'))
