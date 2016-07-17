9b8ce34a2ddd78c56901d3fa8ac9555df627a11b
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-16 23:06:33 -0700

    Directly import methods under test

--- tests/ascoderu_webapp/test_controllers.py
+++ tests/ascoderu_webapp/test_controllers.py
@@ -2,36 +2,40 @@ from datetime import datetime
 
 from flask_testing import TestCase
 
-from ascoderu_webapp import controllers
-from ascoderu_webapp import models
+from ascoderu_webapp.controllers import inbox_emails_for
+from ascoderu_webapp.controllers import new_email_for
+from ascoderu_webapp.controllers import outbox_emails_for
+from ascoderu_webapp.controllers import sent_emails_for
+from ascoderu_webapp.controllers import user_exists
+from ascoderu_webapp.models import Email
 from utils.testing import AppTestMixin
 
 
 class TestUserExists(AppTestMixin, TestCase):
     def test_returns_false_for_invalid_argument(self):
-        self.assertFalse(controllers.user_exists(None))
-        self.assertFalse(controllers.user_exists(''))
+        self.assertFalse(user_exists(None))
+        self.assertFalse(user_exists(''))
 
     def test_finds_user_by_name(self):
         name = 'some-name'
         self.new_user(name=name)
 
-        self.assertTrue(controllers.user_exists(name))
-        self.assertTrue(controllers.user_exists(name.upper()))
+        self.assertTrue(user_exists(name))
+        self.assertTrue(user_exists(name.upper()))
 
     def test_finds_user_by_email(self):
         email = 'someone@email.net'
         self.new_user(email=email)
 
-        self.assertTrue(controllers.user_exists(email))
-        self.assertTrue(controllers.user_exists(email.upper()))
+        self.assertTrue(user_exists(email))
+        self.assertTrue(user_exists(email.upper()))
 
     def test_does_not_find_non_existing_user(self):
         existing_name = 'some-name'
         non_existing_name = 'does-not-exist'
         self.new_user(name=existing_name)
 
-        self.assertFalse(controllers.user_exists(non_existing_name))
+        self.assertFalse(user_exists(non_existing_name))
 
 
 # noinspection PyUnusedLocal
@@ -50,7 +54,7 @@ class TestReadEmails(AppTestMixin, TestCase):
         not_expected = [self.new_email(to=[other_user.name]),
                         self.new_email(to=[other_user.email]),
                         self.new_email(to=[other_user.name.upper()])]
-        actual = controllers.inbox_emails_for(user)
+        actual = inbox_emails_for(user)
 
         self.assertSequenceEqual(actual, expected)
 
@@ -58,7 +62,7 @@ class TestReadEmails(AppTestMixin, TestCase):
         user = self.new_user(name='someone', email='someone@test.net')
         expected = [self.new_email(sender=user.name, date=datetime.now())]
         not_expected = [self.new_email(sender=user.name)]
-        actual = controllers.sent_emails_for(user)
+        actual = sent_emails_for(user)
 
         self.assertSequenceEqual(actual, expected)
 
@@ -66,7 +70,7 @@ class TestReadEmails(AppTestMixin, TestCase):
         user = self.new_user(name='someone', email='someone@test.net')
         expected = [self.new_email(sender=user.name)]
         not_expected = [self.new_email(sender=user.name, date=datetime.now())]
-        actual = controllers.outbox_emails_for(user)
+        actual = outbox_emails_for(user)
 
         self.assertSequenceEqual(actual, expected)
 
@@ -75,8 +79,8 @@ class TestWriteEmails(AppTestMixin, TestCase):
     def test_creates_new_email_for_local_user(self):
         sender = self.new_user(email='sender@test.net')
         recipient = self.new_user(name='recipient', email='recipient@test.net')
-        controllers.new_email_for(sender, recipient.name, 'subject', 'body')
-        email = next(iter(models.Email.query.all()), None)
+        new_email_for(sender, recipient.name, 'subject', 'body')
+        email = next(iter(Email.query.all()), None)
 
         self.assertIsNotNone(email)
         self.assertEqual(sender.email, email.sender)
@@ -86,8 +90,8 @@ class TestWriteEmails(AppTestMixin, TestCase):
     def test_creates_new_email_for_remote_user(self):
         sender = self.new_user(email='sender@test.net')
         recipient = 'recipient@test.net'
-        controllers.new_email_for(sender, recipient, 'subject', 'body')
-        email = next(iter(models.Email.query.all()), None)
+        new_email_for(sender, recipient, 'subject', 'body')
+        email = next(iter(Email.query.all()), None)
 
         self.assertIsNotNone(email)
         self.assertEqual(sender.email, email.sender)
