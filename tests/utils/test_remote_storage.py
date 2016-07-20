637d8d42dc2c7628df7aec670e63fbd303194c43
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-20 15:20:10 -0700

    Add test to document download behavior on 404

--- tests/utils/test_remote_storage.py
+++ tests/utils/test_remote_storage.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 
+from azure.common import AzureMissingResourceHttpError
+
 from utils.remote_storage import AzureBlob
+
+
+class DownloadFailedBlockBlobService(object):
+    @classmethod
+    def get_blob_to_bytes(cls, *args, **kwargs):
+        raise AzureMissingResourceHttpError('download failed', 404)
 
 
 class TestAzureBlob(TestCase):
@@ -8,3 +16,9 @@ class TestAzureBlob(TestCase):
         blob = AzureBlob('account', 'key', 'container', 'up', 'down', 'format')
         blob.conn = lambda: self.fail('called remote service')
         blob.upload(b'')
+
+    def test_missing_download_returns_empty_bytes(self):
+        blob = AzureBlob('account', 'key', 'container', 'up', 'down', 'format')
+        blob.conn = lambda: DownloadFailedBlockBlobService()
+        actual = blob.download()
+        self.assertEqual(actual, b'')
