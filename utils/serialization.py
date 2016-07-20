4a32ae7e788f95cb5f84539701a2ce4931ced35e
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-07-19 19:08:02 -0700

    Use real gzip in CompressedJson serializer
    Using the zlib compressor, we can't operate on the compressed data using
    tools that expect normal gzip data (e.g. zgrep or zless) because zlib
    doesn't include the gzip header in the serialized files. This makes it a
    bit difficult to deal with the serializer output outside of the context
    of the flask app.

--- utils/serialization.py
+++ utils/serialization.py
@@ -1,5 +1,23 @@
+from gzip import GzipFile
+from tempfile import NamedTemporaryFile
 import json
 import zlib
+
+
+def _compress(data, level):
+    with NamedTemporaryFile() as fobj:
+        with GzipFile(fileobj=fobj, mode='wb', compresslevel=level) as gzfobj:
+            gzfobj.write(data)
+        fobj.seek(0)
+        compressed_data = fobj.read()
+    return compressed_data
+
+
+def _decompress(compressed):
+    try:
+        return zlib.decompress(compressed, 16 + zlib.MAX_WBITS)
+    except zlib.error as error:
+        raise ValueError('unable to decompress') from error
 
 
 class CompressedJson(object):
@@ -14,15 +32,15 @@ class CompressedJson(object):
 
         serialized = json.dumps(obj)
         serialized_bytes = serialized.encode(self.ENCODING)
-        return zlib.compress(serialized_bytes, self.level)
+        return _compress(serialized_bytes, level=self.level)
 
     def deserialize(self, compressed):
         if not compressed:
             return None
 
         try:
-            serialized_bytes = zlib.decompress(compressed)
+            serialized_bytes = _decompress(compressed)
             serialized = serialized_bytes.decode(self.ENCODING)
             return json.loads(serialized)
-        except (zlib.error, UnicodeDecodeError, ValueError):
+        except (UnicodeDecodeError, ValueError):
             return None
