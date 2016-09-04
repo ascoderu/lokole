3b65a52e07cc445eafba1534f97b1c29eb82b709
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-09-03 21:18:06 -0700

    Remove property in favor of explicit creation

--- tests/opwen_webapp/helpers/test_serializers.py
+++ tests/opwen_webapp/helpers/test_serializers.py
@@ -35,8 +35,7 @@ class TestRemoteSerializer(FileWritingTestCaseMixin, AppTestMixin, TestCase):
         for actual, expected in zip(actual, expected):
             self.assertTrue(actual.is_same_as(expected))
 
-    @property
-    def serializer(self):
+    def _new_serializer(self):
         """
         :rtype: Serializer
 
@@ -47,29 +46,32 @@ class TestRemoteSerializer(FileWritingTestCaseMixin, AppTestMixin, TestCase):
         return serializer
 
     def test_serialization_deserialization_roundtrip_for_emails(self):
+        serializer = self._new_serializer()
         expected_emails = [self.create_complete_email() for _ in range(5)]
 
-        serialized = self.serializer.serialize(expected_emails)
-        actual_emails, actual_accounts = self.serializer.deserialize(serialized)
+        serialized = serializer.serialize(expected_emails)
+        actual_emails, actual_accounts = serializer.deserialize(serialized)
 
         self.paths_created.add(serialized)
         self.assertEqual(actual_accounts, [])
         self.assertEmailsEqual(actual_emails, expected_emails)
 
     def test_serialization_deserialization_roundtrip_for_accounts(self):
+        serializer = self._new_serializer()
         expected_accounts = [self.create_complete_user() for _ in range(5)]
 
-        serialized = self.serializer.serialize(accounts=expected_accounts)
-        actual_emails, actual_accounts = self.serializer.deserialize(serialized)
+        serialized = serializer.serialize(accounts=expected_accounts)
+        actual_emails, actual_accounts = serializer.deserialize(serialized)
 
         self.paths_created.add(serialized)
         self.assertEqual(actual_emails, [])
         self.assertAccountsEqual(actual_accounts, expected_accounts)
 
     def test_deserializer_handles_corrupt_archive(self):
+        serializer = self._new_serializer()
         serialized = self.new_file(content='not-a-zip-file')
 
-        emails, accounts = self.serializer.deserialize(serialized)
+        emails, accounts = serializer.deserialize(serialized)
 
         self.assertEqual(emails, [])
         self.assertEqual(accounts, [])
