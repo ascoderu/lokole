2d49891e847512a44a17d3f32536f54434b0f649
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-09-17 14:25:50 -0700

    Switch to a less hacky abstract test pattern

--- tests/utils/test_compressor.py
+++ tests/utils/test_compressor.py
@@ -6,61 +6,56 @@ from tests.base import FileWritingTestCaseMixin
 from utils.compressor import ZipCompressor
 
 
-class TestDirectoryCompressor(FileWritingTestCaseMixin, TestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = None
-        if self.__class__ != TestDirectoryCompressor:
-            # noinspection PyUnresolvedReferences
-            self.run = TestCase.run.__get__(self, self.__class__)
-        else:
-            self.run = lambda this, *ar, **kw: None
+class Base(object):
 
-    @abstractproperty
-    def compressor(self):
-        """
-        :rtype: utils.compressor.DirectoryCompressor
+    class TestDirectoryCompressor(FileWritingTestCaseMixin, TestCase):
+        @abstractproperty
+        def compressor(self):
+            """
+            :rtype: utils.compressor.DirectoryCompressor
 
-        """
-        raise NotImplementedError
+            """
+            raise NotImplementedError
 
-    def test_create_archive(self):
-        directory = self.new_directory()
-        self.new_file(directory)
-        self.new_file(directory)
+        def test_create_archive(self):
+            directory = self.new_directory()
+            self.new_file(directory)
+            self.new_file(directory)
 
-        archive = self.compressor.compress(directory)
+            archive = self.compressor.compress(directory)
 
-        self.assertFileExists(archive)
+            self.assertFileExists(archive)
 
-    def test_extract_from_archive(self):
-        content = 'some content'
-        filepath = self.new_file(content=content)
+        def test_extract_from_archive(self):
+            content = 'some content'
+            filepath = self.new_file(content=content)
 
-        archive = self.compressor.compress(path.dirname(filepath))
-        self.assertFileExists(archive)
+            archive = self.compressor.compress(path.dirname(filepath))
+            self.assertFileExists(archive)
 
-        extracted = self.compressor.decompress(
-            archive_path=archive,
-            filename=path.basename(filepath),
-            to_directory=self.new_directory())
-        self.assertFileExists(extracted)
-        self.assertFileContentEqual(extracted, content)
+            extracted = self.compressor.decompress(
+                archive_path=archive,
+                filename=path.basename(filepath),
+                to_directory=self.new_directory())
+            self.assertFileExists(extracted)
+            self.assertFileContentEqual(extracted, content)
 
-    def test_raises_valueerror_on_bad_archive(self):
-        archive = self.new_file(content='not-an-archive')
+        def test_raises_valueerror_on_bad_archive(self):
+            archive = self.new_file(content='not-an-archive')
+            directory = self.new_directory()
 
-        with self.assertRaises(ValueError):
-            self.compressor.decompress(archive, 'file', self.new_directory())
+            with self.assertRaises(ValueError):
+                self.compressor.decompress(archive, 'file', directory)
 
-    def test_raises_keyerror_on_unknown_file(self):
-        filepath = self.new_file(content='some content')
-        archive = self.compressor.compress(path.dirname(filepath))
-        self.paths_created.add(archive)
+        def test_raises_keyerror_on_unknown_file(self):
+            filepath = self.new_file(content='some content')
+            archive = self.compressor.compress(path.dirname(filepath))
+            directory = self.new_directory()
+            self.paths_created.add(archive)
 
-        with self.assertRaises(ValueError):
-            self.compressor.decompress(archive, 'file', self.new_directory())
+            with self.assertRaises(ValueError):
+                self.compressor.decompress(archive, 'file', directory)
 
 
-class TestZipCompressor(TestDirectoryCompressor):
+class TestZipCompressor(Base.TestDirectoryCompressor):
     compressor = ZipCompressor()
