8ae6cf4af57ac8d36dda79d06b7b053c642a4205
Clemens Wolff <clemens.wolff+git@gmail.com>
2016-11-27 17:08:41 -0800

    Include bootstrap in compiled app.css file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-bower-concat');
@@ -8,12 +9,47 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     bower_concat: {
+      options: {
+        separator: ';'
+      },
       all: {
         dest: {
-          js: 'build/all.js',
-          css: 'build/all.css'
+          js: 'build/js/_bower.js',
+          css: 'build/css/_bower.css'
+        },
+        mainFiles: {
+          bootstrap: [
+            'dist/css/bootstrap.css',
+            'dist/js/bootstrap.js'
+          ]
+        },
+        dependencies: {
+          'bootstrap': 'jquery',
+          'bootstrap-fileinput': 'bootstrap',
+          'bootstrap3-wysihtml5': 'bootstrap'
         }
-      },
+      }
+    },
+
+    copy: {
+      bower: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['bower_components/bootstrap/fonts/*'],
+            dest:'opwen_email_client/static/fonts/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['bower_components/components-font-awesome/fonts/*'],
+            dest:'opwen_email_client/static/fonts/',
+            filter: 'isFile'
+          },
+        ]
+      }
     },
 
     cssmin: {
@@ -23,7 +59,9 @@ module.exports = function(grunt) {
       },
       target: {
         files: {
-          'opwen_email_client/static/app.min.css': ['build/all.css']
+          'opwen_email_client/static/css/app.min.css': [
+            'build/css/_bower.css'
+          ]
         }
       }
     },
@@ -35,7 +73,9 @@ module.exports = function(grunt) {
           compress: true
         },
         files: {
-          'opwen_email_client/static/app.min.js': 'build/all.js'
+          'opwen_email_client/static/js/app.min.js': [
+            'build/js/_bower.js'
+          ]
         }
       }
     },
@@ -44,6 +84,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('buildbower', [
     'bower_concat',
+    'copy:bower',
     'cssmin',
     'uglify:bower'
   ]);
