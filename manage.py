e95c13f4e50c86649b3aa422ee2f55acdb05e287
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-02-06 19:41:21 -0800

    Remove references to removed code

--- manage.py
+++ manage.py
@@ -5,11 +5,9 @@ from flask_script import Manager
 
 from opwen_email_client import app
 from opwen_email_client.management import BabelCommand
-from opwen_email_client.management import SyncDaemonCommand
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('babel', BabelCommand)
-manager.add_command('sync-daemon', SyncDaemonCommand)
 
 manager.run()
