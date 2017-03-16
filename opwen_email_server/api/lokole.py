6696451b7c7a9b2de5b624b47159efae8fcf06b7
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-03-15 21:40:23 -0700

    Disable linter in in-progress code

--- opwen_email_server/api/lokole.py
+++ opwen_email_server/api/lokole.py
@@ -3,14 +3,14 @@ def upload(upload_info):
     :type upload_info: dict
 
     """
-    client_id = upload_info['client_id']
-    resource_id = upload_info['resource_id']
-    resource_type = upload_info['resource_type']
+    client_id = upload_info['client_id']  # noqa: F841
+    resource_id = upload_info['resource_id']  # noqa: F841
+    resource_type = upload_info['resource_type']  # noqa: F841
 
     raise NotImplementedError
 
 
-def download(client_id):
+def download(client_id):  # noqa: F841
     """
     :type client_id: str
     :rtype dict
