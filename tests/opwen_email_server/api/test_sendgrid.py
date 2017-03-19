956d948fbc860f068977f03b22597b96cde131bd
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-03-18 17:59:50 -0700

    Make flake8 happy

--- tests/opwen_email_server/api/test_sendgrid.py
+++ tests/opwen_email_server/api/test_sendgrid.py
@@ -7,7 +7,7 @@ from opwen_email_server.api import sendgrid
 class ReceiveTests(TestCase):
     @patch('opwen_email_server.api.sendgrid.QUEUE')
     @patch('opwen_email_server.api.sendgrid.STORAGE')
-    def test_stores_and_processes_incoming_email(self, storage_mock, queue_mock):
+    def test_stores_and_processes_new_email(self, storage_mock, queue_mock):
         sendgrid.receive('some-mime-email')
 
         self.assertEqual(storage_mock.store_text.call_count, 1)
