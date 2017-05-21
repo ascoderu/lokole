277c4b354f0819aea7da48dd1486655773398c64
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-05-20 20:05:53 -0700

    Include translations in package

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = ['manage.py']
 
 setup(
     name='opwen_email_client',
-    version='0.1.0',
+    version='0.1.1',
     author='Clemens Wolff',
     author_email='clemens.wolff+pypi@gmail.com',
     packages=packages,
