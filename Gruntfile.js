cd1231eb588ee88031039151b48188bf1c98a7b5
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-07-22 13:35:55 -0700

    Only include js/css necessary for each page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,9 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-bower-concat');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    bower_concat: {
-      options: {
-        separator: ';'
-      },
-      all: {
-        dest: {
-          js: 'build/js/_bower.js',
-          css: 'build/css/_bower.css'
-        },
-        mainFiles: {
-          bootstrap: [
-            'dist/css/bootstrap.css',
-            'dist/js/bootstrap.js'
-          ]
-        },
-        dependencies: {
-          'bootstrap': 'jquery',
-          'bootstrap-fileinput': 'bootstrap',
-          'bootstrap3-wysihtml5': 'bootstrap'
-        }
-      }
-    },
 
     copy: {
       bower: {
@@ -38,7 +12,7 @@ module.exports = function(grunt) {
             expand: true,
             flatten: true,
             src: ['bower_components/components-font-awesome/fonts/*'],
-            dest:'opwen_email_client/webapp/static/fonts/',
+            dest: 'opwen_email_client/webapp/static/fonts/',
             filter: 'isFile'
           },
           {
@@ -51,51 +25,38 @@ module.exports = function(grunt) {
               'bower_components/flag-icon-css/flags/4x3/tz.svg',
               'bower_components/flag-icon-css/flags/4x3/fr.svg',
             ],
-            dest:'opwen_email_client/webapp/static/flags/4x3/',
-            filter: 'isFile'
+            dest: 'opwen_email_client/webapp/static/flags/4x3/'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: [
+              'bower_components/jquery/dist/jquery.min.js',
+              'bower_components/bootstrap/dist/js/bootstrap.min.js',
+              'bower_components/bootstrap-fileinput/js/fileinput.min.js',
+              'bower_components/bootstrap3-wysihtml5-bower/dist/bootstrap3-wysihtml5.all.min.js',
+            ],
+            dest: 'opwen_email_client/webapp/static/js/'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: [
+              'bower_components/components-font-awesome/css/font-awesome.min.css',
+              'bower_components/flag-icon-css/css/flag-icon.min.css',
+              'bower_components/bootstrap/dist/css/bootstrap.min.css',
+              'bower_components/bootstrap-fileinput/css/fileinput.min.css',
+              'bower_components/bootstrap3-wysihtml5-bower/dist/bootstrap3-wysihtml5.min.css',
+            ],
+            dest: 'opwen_email_client/webapp/static/css/'
           },
         ]
       }
     },
 
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1
-      },
-      target: {
-        files: {
-          'opwen_email_client/webapp/static/css/app.min.css': [
-            'build/css/_bower.css'
-          ]
-        }
-      }
-    },
-
-    uglify: {
-      bower: {
-        options: {
-          mangle: true,
-          compress: true
-        },
-        files: {
-          'opwen_email_client/webapp/static/js/app.min.js': [
-            'build/js/_bower.js'
-          ]
-        }
-      }
-    },
-
   });
 
-  grunt.registerTask('buildbower', [
-    'bower_concat',
-    'copy:bower',
-    'cssmin',
-    'uglify:bower'
-  ]);
-
   grunt.registerTask('default', [
-    'buildbower'
+    'copy:bower',
   ])
 };
