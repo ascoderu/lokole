08bedfcf6f08add0912641993f1fc4f9eadbf7b7
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-07-24 17:54:50 -0700

    Clean up dev server command

--- opwen_email_client/util/management.py
+++ opwen_email_client/util/management.py
@@ -1,5 +1,6 @@
 from glob import glob
 from os.path import join
+from typing import List
 
 # noinspection PyPackageRequirements
 from dotenv import load_dotenv
@@ -11,16 +12,19 @@ from flask_script import Command
 class DevServerCommand(Command):
     def __call__(self, app: Flask):
         _load_environment(app)
-        extra_files = _templates_paths_for(app)
-        app.run(debug=True, extra_files=extra_files)
+
+        templates_paths = _templates_paths_for(app, '*.html')
+        reload_server_if_changed = templates_paths
+
+        app.run(debug=True, extra_files=reload_server_if_changed)
 
 
-def _load_environment(app: Flask):
+def _load_environment(app: Flask) -> None:
     dotenv_path = join(app.root_path, '..', '..', '.env')
     load_dotenv(dotenv_path)
 
 
-def _templates_paths_for(app: Flask, templates_matcher: str='*.html'):
+def _templates_paths_for(app: Flask, templates_matcher: str) -> List[str]:
     templates_directory = join(app.root_path, app.template_folder)
     templates_glob = join(templates_directory, '**', templates_matcher)
     return glob(templates_glob, recursive=True)
