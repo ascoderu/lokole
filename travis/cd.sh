38e12262287e24773dab152bfe723dc059599a12
Clemens Wolff <clewolff@microsoft.com>
2017-11-09 10:59:36 -0500

    Don't trigger nounset pre environment validation

--- travis/cd.sh
+++ travis/cd.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 if [ -z "$TRAVIS_TAG" ]; then
   echo "Build is not a release, skipping CD" >&2; exit 0
 fi
@@ -9,6 +7,8 @@ fi
 if [ -z "$DOCKERIO_USERNAME" -o -z "$DOCKERIO_PASSWORD" ]; then
   echo "No docker.io credentials configured, unable to publish builds" >&2; exit 1
 fi
+
+set -euo pipefail
 
 export APP_PORT="80"
 export BUILD_TAG="$TRAVIS_TAG"
