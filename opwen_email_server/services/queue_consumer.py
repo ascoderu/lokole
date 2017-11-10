9f785142b049a3537259b8c1b48ab0e27cb55955
Clemens Wolff <clewolff@microsoft.com>
2017-11-10 10:41:05 -0500

    Add healthcheck to job container

--- opwen_email_server/services/queue_consumer.py
+++ opwen_email_server/services/queue_consumer.py
@@ -4,6 +4,8 @@ from typing import Iterable
 
 from opwen_email_server.azure_constants import QUEUE_POLL_INTERVAL
 from opwen_email_server.utils.log import LogMixin
+from opwen_email_server.config import QUEUE_ERROR_FILE
+from opwen_email_server.utils.temporary import remove_if_exists
 
 
 class QueueConsumer(LogMixin):
@@ -29,8 +31,21 @@ class QueueConsumer(LogMixin):
             # noinspection PyBroadException
             try:
                 self.run_once()
-            except Exception:
-                self.log_exception('error polling queue')
-                pass
-            self.log_debug('done polling queue')
+            except Exception as ex:
+                self._report_error(ex)
+            else:
+                self._report_success()
             sleep(self._poll_seconds)
+
+    def _report_success(self):
+        self.log_debug('done polling queue')
+
+        if QUEUE_ERROR_FILE:
+            remove_if_exists(QUEUE_ERROR_FILE)
+
+    def _report_error(self, ex: Exception):
+        self.log_exception('error polling queue')
+
+        if QUEUE_ERROR_FILE:
+            with open(QUEUE_ERROR_FILE, 'a') as fobj:
+                fobj.write('{}\n'.format(ex))
