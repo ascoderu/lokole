2bcea664c34fdb663ff43427f4cb5c02df9a4543
Clemens Wolff <clewolff@microsoft.com>
2017-11-17 10:29:26 -0500

    Wire-up CD to PyPI via Travis

---
+++ travis/cd.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [ -z "$TRAVIS_TAG" ]; then
+  echo "Build is not a release, skipping CD" >&2; exit 0
+fi
+
+if [ -z "$PYPI_USERNAME" -o -z "$PYPI_PASSWORD" ]; then
+  echo "No PyPI credentials configured, unable to publish builds" >&2; exit 1
+fi
+
+cleanup() {
+  rm -f ~/.pypirc
+}
+trap cleanup EXIT
+
+cat > ~/.pypirc << EOF
+[distutils]
+index-servers =
+    pypi
+
+[pypi]
+username: $PYPI_USERNAME
+password: $PYPI_PASSWORD
+EOF
+
+echo "$TRAVIS_TAG" > version.txt
+
+py_env=~/virtualenv/python$TRAVIS_PYTHON_VERSION
+python="$py_env/bin/python"
+
+${python} setup.py sdist upload
