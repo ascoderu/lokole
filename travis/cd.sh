38687ba148e9e28dd83c02cb1f076c57c03fadac
Clemens Wolff <clewolff@microsoft.com>
2017-11-17 11:22:57 -0500

    Fix shellcheck errors

--- travis/cd.sh
+++ travis/cd.sh
@@ -6,7 +6,7 @@ if [ -z "$TRAVIS_TAG" ]; then
   echo "Build is not a release, skipping CD" >&2; exit 0
 fi
 
-if [ -z "$DOCKER_USERNAME" -o -z "$DOCKER_PASSWORD" ]; then
+if [ -z "$DOCKER_USERNAME" ] || [ -z "$DOCKER_PASSWORD" ]; then
   echo "No docker credentials configured, unable to publish builds" >&2; exit 1
 fi
 
@@ -15,7 +15,7 @@ compose_file="$(mktemp)"
 env_file='.env'
 cert_file='cert.pem'
 
-openssl aes-256-cbc -K $encrypted_2ff31a343d6c_key -iv $encrypted_2ff31a343d6c_iv -in travis/secrets.tar.enc -out "$secrets_archive" -d
+openssl aes-256-cbc -K "$encrypted_2ff31a343d6c_key" -iv "$encrypted_2ff31a343d6c_iv" -in travis/secrets.tar.enc -out "$secrets_archive" -d
 tar xf "$secrets_archive" -C . "$cert_file" "$env_file"
 touch "$env_file"
 
@@ -32,15 +32,15 @@ docker-compose -f "$compose_file" build
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 docker-compose -f "$compose_file" push
 
-if [ -z "$SERVICE_FABRIC_HOST" -o -z "$SERVICE_FABRIC_DEPLOYMENT_NAME" ]; then
+if [ -z "$SERVICE_FABRIC_HOST" ] || [ -z "$SERVICE_FABRIC_DEPLOYMENT_NAME" ]; then
   echo "No service fabric credentials configured, skipping upgrade of cluster" >&2; exit 0
 fi
 
-if [ ! -f "$cert_file" -o ! -s "$env_file" ]; then
+if [ ! -f "$cert_file" ] || [ ! -s "$env_file" ]; then
   echo "No service fabric secrets found, unable to upgrade cluster" >&2; exit 2
 fi
 
-py_env=~/virtualenv/python$TRAVIS_PYTHON_VERSION
+py_env="$HOME/virtualenv/python$TRAVIS_PYTHON_VERSION"
 pip="$py_env/bin/pip"
 sfctl="$py_env/bin/sfctl"
 
