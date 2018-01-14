e01d81584796e0898daea60ac2c23a19af05f911
Clemens Wolff <clewolff@microsoft.com>
2018-01-14 05:26:34 -0500

    Add test for appinsights being called

--- tests/opwen_email_server/utils/test_log.py
+++ tests/opwen_email_server/utils/test_log.py
@@ -26,9 +26,17 @@ class LogMixinTests(TestCase):
         self.assertDidLog('info', '%s:a %s:b %s:message %d', 'Bar', 1, 2, 3)
 
     def assertDidLog(self, log_level, *log_args):
-        log_method_mock = getattr(self.logger_mock, log_level)
-        log_method_mock.assert_called_once_with(*log_args)
         self.assertIsLogMessage(*log_args)
+        self.assertDidCallLogger(log_level, *log_args)
+        self.assertDidCallApplicationInsights(log_level, *log_args)
+
+    def assertDidCallApplicationInsights(self, log_level, fmt, *args):
+        mock = self.appinsights_mock.track_trace
+        mock.assert_called_once_with(fmt % args, {'level': log_level})
+
+    def assertDidCallLogger(self, log_level, *log_args):
+        mock = getattr(self.logger_mock, log_level)
+        mock.assert_called_once_with(*log_args)
 
     def assertIsLogMessage(self, fmt, *args):
         try:
@@ -38,5 +46,8 @@ class LogMixinTests(TestCase):
 
     def setUp(self):
         self.log_patcher = patch.object(log, '_LOG')
+        self.appinsights_patcher = patch.object(log, '_APPINSIGHTS')
         self.logger_mock = self.log_patcher.start()
+        self.appinsights_mock = self.appinsights_patcher.start()
         self.addCleanup(self.log_patcher.stop)
+        self.addCleanup(self.appinsights_patcher.stop)
