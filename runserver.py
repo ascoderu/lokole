45b5540d9796d564b45560a71942e1dd6cde981f
Clemens Wolff <clewolff@microsoft.com>
2018-05-27 18:31:03 -0400

    Remove waitress support

--- runserver.py
+++ runserver.py
@@ -11,7 +11,6 @@ _server = _servers[0]
 _host = _hosts[0]
 _port = 8080
 _ui = False
-_waitress = False
 
 
 def build_app(apis, host=_host, port=_port, server=_server, ui=_ui):
@@ -41,16 +40,10 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=int, default=_port)
     parser.add_argument('--server', choices=_servers, default=_server)
     parser.add_argument('--ui', action='store_true', default=_ui)
-    if can_import('waitress'):
-        parser.add_argument('--waitress', action='store_true', default=_waitress)
     parser.add_argument('apis', nargs='+', type=FileType('r'))
     args = parser.parse_args()
 
     app = build_app([api.name for api in args.apis], args.host,
                     args.port, args.server, args.ui)
 
-    if can_import('waitress') and args.waitress:
-        from waitress import serve
-        serve(app.app, host=args.host, port=args.port)
-    else:
-        app.run()
+    app.run()
