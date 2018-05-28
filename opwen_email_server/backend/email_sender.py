73c1900a05fa3e4f68224f4e0d5dce2c08687254
Clemens Wolff <clewolff@microsoft.com>
2018-05-28 13:57:54 -0400

    Add attachment support to email sender CLI

--- opwen_email_server/backend/email_sender.py
+++ opwen_email_server/backend/email_sender.py
@@ -22,14 +22,25 @@ def send(email: dict) -> Tuple[str, int]:
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
+    from argparse import FileType
+    from base64 import b64encode
     from json import loads
+    from os.path import basename
     from uuid import uuid4
 
     parser = ArgumentParser()
     parser.add_argument('email')
+    parser.add_argument('--attachment', type=FileType('rb'))
     args = parser.parse_args()
 
     email = loads(args.email)
     email.setdefault('_uid', str(uuid4()))
+
+    if args.attachment:
+        email.setdefault('attachments', []).append({
+            'filename': basename(args.attachment.name),
+            'content': b64encode(args.attachment.read()).decode('ascii')
+        })
+        args.attachment.close()
 
     send(email)
