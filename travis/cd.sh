224a46af72388058937ea7326374482cd45c8d63
Clemens Wolff <clewolff@microsoft.com>
2018-05-28 17:12:07 -0400

    Update PyPI release to use twine

--- travis/cd.sh
+++ travis/cd.sh
@@ -14,32 +14,15 @@ if [ -z "$DOCKER_USERNAME" ] || [ -z "$DOCKER_PASSWORD" ]; then
   echo "No docker credentials configured, unable to publish builds" >&2; exit 1
 fi
 
-cleanup() {
-  rm -f ~/.pypirc
-}
-trap cleanup EXIT
-
-cat > ~/.pypirc << EOF
-[distutils]
-index-servers =
-    pypi
-
-[pypi]
-username: $PYPI_USERNAME
-password: $PYPI_PASSWORD
-EOF
-
 echo "$TRAVIS_TAG" > version.txt
 
 py_env="$HOME/virtualenv/python$TRAVIS_PYTHON_VERSION"
-python="$py_env/bin/python"
 
 make prepare-server -e py_env="$py_env" -e NPM="$(which npm)" -e SHELLCHECK="$(which shellcheck)"
 
-while ! ${python} setup.py sdist upload; do
-  echo "Unable to upload to PyPI, retrying" >&2
-  sleep 1m
-done
+"$py_env/bin/pip" install twine
+"$py_env/bin/python" setup.py sdist
+"$py_env/bin/twine" upload -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD" dist/*
 
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
