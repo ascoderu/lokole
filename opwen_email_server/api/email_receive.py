c39cd23c08e314943fa42112aa315e136b863913
Adam Sclafani <adam.sclafani15@gmail.com>
2018-09-30 15:13:10 -0700

    further file permissions tweaking

--- opwen_email_server/api/email_receive.py
+++ opwen_email_server/api/email_receive.py
@@ -5,19 +5,15 @@ from opwen_email_server import azure_constants as constants
 from opwen_email_server import config
 from opwen_email_server import events
 from opwen_email_server.services.auth import AzureAuth
-from opwen_email_server.services.queue import AzureQueue
 from opwen_email_server.services.storage import AzureTextStorage
 from opwen_email_server.utils.log import LogMixin
+
+from opwen_email_server.celery import celery
 
 STORAGE = AzureTextStorage(account=config.BLOBS_ACCOUNT,
                            key=config.BLOBS_KEY,
                            container=constants.CONTAINER_SENDGRID_MIME,
                            provider=config.STORAGE_PROVIDER)
-
-QUEUE = AzureQueue(namespace=config.QUEUES_NAMESPACE,
-                   sas_key=config.QUEUES_SAS_KEY,
-                   sas_name=config.QUEUES_SAS_NAME,
-                   name=constants.QUEUE_SENDGRID_MIME)
 
 CLIENTS = AzureAuth(
     storage=AzureTextStorage(
@@ -25,7 +21,6 @@ CLIENTS = AzureAuth(
         key=config.TABLES_KEY,
         container=constants.TABLE_AUTH,
         provider=config.STORAGE_PROVIDER))
-
 
 class _Receiver(LogMixin):
     def __call__(self, client_id: str, email: str) -> Tuple[str, int]:
@@ -38,12 +33,7 @@ class _Receiver(LogMixin):
 
         STORAGE.store_text(email_id, email)
 
-        QUEUE.enqueue({
-            '_version': '0.1',
-            '_type': 'mime_email_received',
-            'resource_id': email_id,
-            'container_name': STORAGE.container,
-        })
+        celery.inbound_store.delay(email_id)
 
         self.log_event(events.EMAIL_RECEIVED_FOR_CLIENT, {'domain': domain})  # noqa: E501
         return 'received', 200
