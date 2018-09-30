3e8b75a284fc415c197137bfd00c454ea02651b9
Clemens Wolff <clewolff@microsoft.com>
2018-09-30 18:22:30 -0400

    Restore dependency-injection style classes

--- opwen_email_server/api/store_written_client_emails.py
+++ opwen_email_server/api/store_written_client_emails.py
@@ -2,12 +2,11 @@ from opwen_email_server import azure_constants as constants
 from opwen_email_server import config
 from opwen_email_server import events
 from opwen_email_server.backend import server_datastore
+from opwen_email_server.celery import celery
 from opwen_email_server.services.storage import AzureFileStorage
 from opwen_email_server.services.storage import AzureObjectStorage
 from opwen_email_server.utils.email_parser import get_domain
 from opwen_email_server.utils.log import LogMixin
-
-from opwen_email_server.celery import celery
 
 STORAGE = AzureObjectStorage(
     file_storage=AzureFileStorage(
@@ -16,24 +15,26 @@ STORAGE = AzureObjectStorage(
         container=constants.CONTAINER_CLIENT_PACKAGES,
         provider=config.STORAGE_PROVIDER))
 
-logger = LogMixin()
 
-def store(resource_id: str):
+class _WrittenStorer(LogMixin):
+    def __call__(self, resource_id: str):
+        emails = STORAGE.fetch_objects(resource_id)
 
-    emails = STORAGE.fetch_objects(resource_id)
+        domain = ''
+        num_stored = 0
+        for email in emails:
+            email_id = email['_uid']
+            server_datastore.store_outbound_email(email_id, email)
 
-    domain = ''
-    num_stored = 0
-    for email in emails:
-        email_id = email['_uid']
-        server_datastore.store_outbound_email(email_id, email)
+            celery.send.delay(email_id)
 
-        celery.send.delay(email_id)
+            num_stored += 1
+            domain = get_domain(email.get('from', ''))
 
-        num_stored += 1
-        domain = get_domain(email.get('from', ''))
+        STORAGE.delete(resource_id)
 
-    STORAGE.delete(resource_id)
+        self.log_event(events.EMAIL_STORED_FROM_CLIENT, {'domain': domain, 'num_emails': num_stored})  # noqa: E501
+        return 'OK', 200
 
-    logger.log_event(events.EMAIL_STORED_FROM_CLIENT, {'domain': domain, 'num_emails': num_stored})  # noqa: E501
-    return 'OK', 200
+
+store = _WrittenStorer()
