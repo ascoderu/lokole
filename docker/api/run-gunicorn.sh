eb0293c9fe6523b9b95c51b748c0b2ff6872e0c8
Clemens Wolff <clewolff@microsoft.com>
2018-09-30 19:17:48 -0400

    Enable one server for all APIs in docker-compose

--- docker/api/run-gunicorn.sh
+++ docker/api/run-gunicorn.sh
@@ -1,16 +1,23 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-api_spec="/app/${CONNEXION_SPEC}"
-healthcheck_spec="/app/opwen_email_server/static/healthcheck-spec.yaml"
+declare -a api_spec_paths
+api_spec_paths=(${CONNEXION_SPEC//,/ })
+api_spec_paths+=("opwen_email_server/static/healthcheck-spec.yaml")
 
-for spec in "${api_spec}" "${healthcheck_spec}"; do
-  if [ ! -f "${spec}" ]; then
-    echo "Unable to start server: connexion spec file ${spec} does not exist" >&2
+apis=""
+for api_spec_path in "${api_spec_paths[@]}"; do
+  api_spec_path="/app/${api_spec_path}"
+
+  if [ ! -f "${api_spec_path}" ]; then
+    echo "Unable to start server: connexion spec file ${api_spec_path} does not exist" >&2
     exit 1
   fi
+
+  apis="${apis},'${api_spec_path}'"
 done
+apis="[${apis:1:${#apis}-1}]"
 
 /venv/bin/gunicorn \
   --workers="${GUNICORN_WORKERS}" \
   --bind="0.0.0.0:80" \
-  "server:build_app(apis=['${api_spec}','${healthcheck_spec}'], server='tornado', ui=${TESTING_UI})"
+  "server:build_app(apis=${apis}, server='tornado', ui=${TESTING_UI})"
--- docker/nginx/healthcheck.sh
+++ docker/nginx/healthcheck.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-if ! curl --fail --silent "http://apiemailreceive/healthcheck/ping"; then
-  echo "Unable to call apiemailreceive" >&2
+if ! curl --fail --silent "http://${HOSTNAME_EMAIL_RECEIVE}/healthcheck/ping"; then
+  echo "Unable to call ${HOSTNAME_EMAIL_RECEIVE}" >&2
   exit 1
 fi
 
-if ! curl --fail --silent "http://apiclientwrite/healthcheck/ping"; then
-  echo "Unable to call apiclientwrite" >&2
+if ! curl --fail --silent "http://${HOSTNAME_CLIENT_WRITE}/healthcheck/ping"; then
+  echo "Unable to call ${HOSTNAME_CLIENT_WRITE}" >&2
   exit 2
 fi
 
-if ! curl --fail --silent "http://apiclientread/healthcheck/ping"; then
-  echo "Unable to call apiclientread" >&2
+if ! curl --fail --silent "http://${HOSTNAME_CLIENT_READ}/healthcheck/ping"; then
+  echo "Unable to call ${HOSTNAME_CLIENT_READ}" >&2
   exit 3
 fi
 
