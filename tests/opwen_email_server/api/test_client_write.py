cea7b75894bd78b0051b37a56ab0e95b19a23c14
Clemens Wolff <clewolff@microsoft.com>
2018-09-30 18:33:31 -0400

    Fix unit tests

--- tests/opwen_email_server/api/test_client_write.py
+++ tests/opwen_email_server/api/test_client_write.py
@@ -6,12 +6,12 @@ from tests.opwen_email_server.api.api_test_base import AuthTestMixin
 
 
 class UploadTests(TestCase, AuthTestMixin):
-    @patch.object(client_write, 'QUEUE')
+    @patch.object(client_write, 'celery')
     def test_schedules_upload_for_client(self, mock_queue):
         with self.given_clients(client_write, {'client1': 'id1'}):
             message, status = client_write.upload('client1', {})
             self.assertEqual(status, 200)
-            self.assertEqual(mock_queue.enqueue.call_count, 1)
+            self.assertEqual(mock_queue.written_store.delay.call_count, 1)
 
     def test_denies_unknown_client(self):
         with self.given_clients(client_write, {'client1': 'id1'}):
--- tests/opwen_email_server/api/test_store_written_client_emails.py
+++ tests/opwen_email_server/api/test_store_written_client_emails.py
@@ -6,7 +6,7 @@ from opwen_email_server.api import store_written_client_emails
 
 
 class StoreWrittenClientEmailsTests(TestCase):
-    @patch.object(store_written_client_emails, 'QUEUE')
+    @patch.object(store_written_client_emails, 'celery')
     @patch.object(store_written_client_emails, 'STORAGE')
     @patch.object(server_datastore, 'store_outbound_email')
     def test_reads_message_and_stores_email(
@@ -23,7 +23,7 @@ class StoreWrittenClientEmailsTests(TestCase):
 
         self.assertEqual(client_storage_mock.fetch_objects.call_count, 1)
         self.assertEqual(client_storage_mock.delete.call_count, 1)
-        self.assertEqual(send_queue_mock.enqueue.call_count, 2)
+        self.assertEqual(send_queue_mock.send.delay.call_count, 2)
         self.assertEqual(store_mock.call_count, 2)
         store_mock.assert_any_call(email1_id, email1)
         store_mock.assert_any_call(email2_id, email2)
