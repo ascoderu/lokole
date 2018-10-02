1241e2e777cdcbc5c197f624ca78b8a21996fe84
Clemens Wolff <clewolff@microsoft.com>
2018-10-02 19:26:23 -0400

    Fix bad rename

--- docker/worker/run-celery.sh
+++ docker/worker/run-celery.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 /venv/bin/celery \
-  --app="opwen_email_server.services.celery"
+  --app="opwen_email_server.services.tasks" \
   worker \
   --concurrency="${CELERY_WORKERS}" \
   --loglevel="${LOKOLE_LOG_LEVEL}"
