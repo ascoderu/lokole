63fdbbd481cfb59fa68a54ae93a1823a1451406c
Clemens Wolff <clewolff@microsoft.com>
2018-11-25 15:26:20 -0500

    Add logging to DNS setup

--- opwen_email_server/integration/dns.py
+++ opwen_email_server/integration/dns.py
@@ -4,6 +4,7 @@ from opwen_email_server.config import CLOUDFLARE_KEY
 from opwen_email_server.config import CLOUDFLARE_USER
 from opwen_email_server.config import CLOUDFLARE_ZONE
 from opwen_email_server.config import SENDGRID_KEY
+from opwen_email_server.utils.log import LogMixin
 
 SENDGRID_URL = 'https://api.sendgrid.com/v3/user/webhooks/parse/settings'
 
@@ -13,36 +14,53 @@ CLOUDFLARE_URL = 'https://api.cloudflare.com/client/v4/zones' \
                  '/{}/dns_records'.format(CLOUDFLARE_ZONE)
 
 
-def setup_email_dns(client_id: str, domain: str):
-    client_name = domain.split('.')[0]
+class SetupEmailDns(LogMixin):
+    def __call__(self, client_id: str, domain: str):
+        self._configure_mailbox(client_id, domain)
+        self._configure_mx_record(domain)
 
-    requests.post(
-        url=SENDGRID_URL,
-        json={
-            'hostname': domain,
-            'url': INBOX_URL.format(client_id),
-            'spam_check': True,
-            'send_raw': True,
-        },
-        headers={
-            'Authorization': 'Bearer {}'.format(SENDGRID_KEY),
-        }
-    ).raise_for_status()
+    def _configure_mailbox(self, client_id: str, domain: str):
+        if not SENDGRID_KEY:
+            self.log_info('No key set, skipping mailbox setup for %s', domain)
+            return
 
-    requests.post(
-        url=CLOUDFLARE_URL,
-        json={
-            'type': 'MX',
-            'content': 'mx.sendgrid.net',
-            'proxied': False,
-            'priority': 1,
-            'name': client_name,
-        },
-        headers={
-            'X-Auth-Key': CLOUDFLARE_KEY,
-            'X-Auth-Email': CLOUDFLARE_USER,
-        }
-    ).raise_for_status()
+        requests.post(
+            url=SENDGRID_URL,
+            json={
+                'hostname': domain,
+                'url': INBOX_URL.format(client_id),
+                'spam_check': True,
+                'send_raw': True,
+            },
+            headers={
+                'Authorization': 'Bearer {}'.format(SENDGRID_KEY),
+            }
+        ).raise_for_status()
+
+    def _configure_mx_record(self, domain: str):
+        if not CLOUDFLARE_KEY:
+            self.log_info('No key set, skipping MX setup for %s', domain)
+            return
+
+        client_name = domain.split('.')[0]
+
+        requests.post(
+            url=CLOUDFLARE_URL,
+            json={
+                'type': 'MX',
+                'content': 'mx.sendgrid.net',
+                'proxied': False,
+                'priority': 1,
+                'name': client_name,
+            },
+            headers={
+                'X-Auth-Key': CLOUDFLARE_KEY,
+                'X-Auth-Email': CLOUDFLARE_USER,
+            }
+        ).raise_for_status()
+
+
+setup_email_dns = SetupEmailDns()
 
 
 def _cli():
