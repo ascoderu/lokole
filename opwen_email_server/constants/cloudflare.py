20055a7471fe272ca19b95e1f660b0ef4b388f7f
Clemens Wolff <clewolff@microsoft.com>
2018-11-25 22:45:41 -0500

    Move dns setup to cloudflare module

---
+++ opwen_email_server/constants/cloudflare.py
@@ -0,0 +1,3 @@
+from typing_extensions import Final  # noqa: F401
+
+DNS_URL = 'https://api.cloudflare.com/client/v4/zones/{}/dns_records'  # type: Final  # noqa: E501
--- opwen_email_server/integration/dns.py
+++ opwen_email_server/integration/dns.py
@@ -1,42 +1,16 @@
-import requests
-
 from opwen_email_server.config import CLOUDFLARE_KEY
 from opwen_email_server.config import CLOUDFLARE_USER
 from opwen_email_server.config import CLOUDFLARE_ZONE
 from opwen_email_server.config import SENDGRID_KEY
+from opwen_email_server.services.cloudflare import SetupCloudflareMxRecords
 from opwen_email_server.services.sendgrid import SetupSendgridMailbox
 from opwen_email_server.utils.log import LogMixin
-
-CLOUDFLARE_URL = 'https://api.cloudflare.com/client/v4/zones' \
-                 '/{}/dns_records'.format(CLOUDFLARE_ZONE)
 
 
 class SetupEmailDns(LogMixin):
     def __call__(self, client_id: str, domain: str):
         SetupSendgridMailbox(SENDGRID_KEY)(client_id, domain)
-        self._configure_mx_record(domain)
-
-    def _configure_mx_record(self, domain: str):
-        if not CLOUDFLARE_KEY:
-            self.log_warning('No key, skipping MX setup for %s', domain)
-            return
-
-        client_name = domain.split('.')[0]
-
-        requests.post(
-            url=CLOUDFLARE_URL,
-            json={
-                'type': 'MX',
-                'content': 'mx.sendgrid.net',
-                'proxied': False,
-                'priority': 1,
-                'name': client_name,
-            },
-            headers={
-                'X-Auth-Key': CLOUDFLARE_KEY,
-                'X-Auth-Email': CLOUDFLARE_USER,
-            }
-        ).raise_for_status()
+        SetupCloudflareMxRecords(CLOUDFLARE_USER, CLOUDFLARE_KEY, CLOUDFLARE_ZONE)(domain)
 
 
 def _cli():
