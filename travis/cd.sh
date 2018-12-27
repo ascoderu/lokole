fd9784165c10cb80b4aa8fcddb166a9eb75b2f4c
Clemens Wolff <clemens@justamouse.com>
2018-12-27 18:07:15 -0500

    Remove deployment to PyPI (#98)
    Previously opwen_email_server was pip-installed by the client to access
    the registration script. Registration was changed to a service call so
    this is no longer required -- as such, we don't have to deploy
    opwen_email_server to PyPI anymore.

--- travis/cd.sh
+++ travis/cd.sh
@@ -10,22 +10,11 @@ if [[ -z "$DOCKER_USERNAME" ]] || [[ -z "$DOCKER_PASSWORD" ]]; then
   echo "No docker credentials configured, unable to publish builds" >&2; exit 1
 fi
 
-if [[ -z "$PYPI_USERNAME" ]] || [[ -z "$PYPI_PASSWORD" ]]; then
-  echo "No PyPI credentials configured, unable to publish builds" >&2; exit 1
-fi
-
-#
-# setup
-#
-
 #
 # docker deploy
 #
 
 docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
-
-touch "./secrets/azure.env"
-touch "./secrets/sendgrid.env"
 
 for tag in "latest" "$TRAVIS_TAG"; do
   BUILD_TAG="$tag" DOCKER_REPO="$DOCKER_USERNAME" USE_DEVTOOLS="False" docker-compose build
@@ -34,18 +23,6 @@ for tag in "latest" "$TRAVIS_TAG"; do
   docker build -t "$DOCKER_USERNAME/opwenserver_setup:$tag" -f "./docker/setup/Dockerfile" "."
   docker push "$DOCKER_USERNAME/opwenserver_setup:$tag"
 done
-
-#
-# pypi deploy
-#
-
-py_env="$HOME/virtualenv/python$TRAVIS_PYTHON_VERSION"
-
-echo "$TRAVIS_TAG" > version.txt
-
-"$py_env/bin/pip" install twine
-"$py_env/bin/python" setup.py sdist
-"$py_env/bin/twine" upload -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD" dist/*
 
 #
 # production deploy
