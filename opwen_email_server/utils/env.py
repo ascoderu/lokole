51865bd08ff2e5d8020db3f27d35d954209a40e6
Clemens Wolff <clemens@justamouse.com>
2018-12-27 22:11:26 -0500

    Extract environment access class (#104)
    * Extract environment access class
    
    * Fix Celery/ServiceBus dependency

---
+++ opwen_email_server/utils/env.py
@@ -0,0 +1,18 @@
+from os import environ
+from typing import Dict
+from typing import Optional
+from urllib.parse import quote
+
+
+class Env(object):
+    def __init__(self, env: Optional[Dict[str, str]] = None):
+        self._env = env or environ
+
+    def __call__(self, name: str, default: str = '') -> str:
+        return self._env.get(name, default)
+
+    def integer(self, name: str, default: int = 0) -> int:
+        return int(self(name, str(default)))
+
+    def urlpart(self, name: str, default: str = '') -> str:
+        return quote(self(name, default), safe='')
---
+++ tests/opwen_email_server/utils/test_env.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from opwen_email_server.utils.env import Env
+
+
+class EnvTests(TestCase):
+    def test_get_string(self):
+        env = Env({'foo': 'bar'})
+        self.assertEqual(env('foo'), 'bar')
+        self.assertEqual(env('missing'), '')
+
+    def test_get_integer(self):
+        env = Env({'foo': '123'})
+        self.assertEqual(env.integer('foo'), 123)
+        self.assertEqual(env.integer('missing'), 0)
+
+    def test_get_urlpart(self):
+        env = Env({'foo': 'ba/r'})
+        self.assertEqual(env.urlpart('foo'), 'ba%2Fr')
+        self.assertEqual(env.urlpart('missing'), '')
