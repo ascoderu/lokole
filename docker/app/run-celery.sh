ac5551bcb96dc4117384de9c107e912377dd4606
Clemens Wolff <clewolff@microsoft.com>
2018-12-27 20:25:36 -0500

    Make queue names consistent with ARM template

--- docker/app/run-celery.sh
+++ docker/app/run-celery.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ "${CELERY_QUEUE_NAMES}" = "all" ]]; then
+  CELERY_QUEUE_NAMES="inbound,written,send"
+fi
+
 "${PY_ENV}/bin/celery" \
   --app="opwen_email_server.integration.celery" \
   worker \
