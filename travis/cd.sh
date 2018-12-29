6a8b325cd3ec2b42690b2622a8f07a31c1faa61d
Clemens Wolff <clemens@justamouse.com>
2018-12-29 13:35:49 -0500

    Remove unused docker publish (#99)

--- travis/cd.sh
+++ travis/cd.sh
@@ -10,10 +10,6 @@ if [[ -z "$PYPI_USERNAME" ]] || [[ -z "$PYPI_PASSWORD" ]]; then
   echo "No PyPI credentials configured, unable to publish builds" >&2; exit 1
 fi
 
-if [[ -z "$DOCKER_USERNAME" ]] || [[ -z "$DOCKER_PASSWORD" ]]; then
-  echo "No docker credentials configured, unable to publish builds" >&2; exit 1
-fi
-
 echo "$TRAVIS_TAG" > version.txt
 
 py_env="$HOME/virtualenv/python$TRAVIS_PYTHON_VERSION"
@@ -23,15 +19,3 @@ make prepare-server -e py_env="$py_env"
 "$py_env/bin/pip" install twine
 "$py_env/bin/python" setup.py sdist
 "$py_env/bin/twine" upload -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD" dist/*
-
-docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
-
-docker build \
-  --build-arg "CLIENT_VERSION=$TRAVIS_TAG" \
-  --tag "$DOCKER_USERNAME/opwenclient_app:$TRAVIS_TAG" \
-  docker/app
-
-docker push "$DOCKER_USERNAME/opwenclient_app:$TRAVIS_TAG"
-
-docker tag "$DOCKER_USERNAME/opwenclient_app:$TRAVIS_TAG" "$DOCKER_USERNAME/opwenclient_app:latest"
-docker push "$DOCKER_USERNAME/opwenclient_app:latest"
