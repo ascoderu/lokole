8f25293a3f1dee101227b09841239f0446b3826f
Clemens Wolff <clemens@justamouse.com>
2018-12-29 19:42:01 -0500

    Add HTTPS to deployment (#108)
    * Add switches to skip deployment steps
    
    * Handle Windows newlines in dotenv files
    
    * Automate A-record DNS setup after deployment
    
    * Add dedicated ingress to k8s deployment
    
    * Add HTTPS to deployment
    
    * Simplify nginx configuration

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -23,6 +23,7 @@
 ##   KUBERNETES_DOCKER_TAG
 ##   KUBERNETES_NODE_SKU
 ##   KUBERNETES_NODE_COUNT
+##   LOKOLE_DNS_NAME
 ##
 
 scriptdir="$(dirname "$0")"
@@ -50,14 +51,11 @@ az account set --subscription "${SUBSCRIPTION_ID}"
 az configure --defaults location="${LOCATION}"
 
 #
-# setup resource group
-#
-
-use_resource_group "${RESOURCE_GROUP_NAME}"
-
-#
 # setup azure resources
 #
+if [[ "${DEPLOY_SERVICES}" != "no" ]]; then
+
+use_resource_group "${RESOURCE_GROUP_NAME}"
 
 storageaccountsku="${STORAGE_ACCOUNT_SKU:-Standard_GRS}"
 servicebussku="${SERVICE_BUS_SKU:-Basic}"
@@ -86,9 +84,12 @@ LOKOLE_EMAIL_SERVER_QUEUES_SAS_NAME=$(jq -r .properties.outputs.serverQueuesSasN
 LOKOLE_EMAIL_SERVER_QUEUES_SAS_KEY=$(jq -r .properties.outputs.serverQueuesSasKey.value /tmp/deployment.json)
 EOF
 
+fi
+
 #
 # create production deployment
 #
+if [[ "${DEPLOY_COMPUTE}" != "no" ]]; then
 
 if [[ -z "${KUBERNETES_RESOURCE_GROUP_NAME}" ]] || [[ -z "${KUBERNETES_NODE_COUNT}" ]] || [[ -z "${KUBERNETES_NODE_SKU}" ]]; then
   log "Skipping production deployment to kubernetes since KUBERNETES_RESOURCE_GROUP_NAME, KUBERNETES_NODE_COUNT, or KUBERNETES_NODE_SKU are not set"
@@ -127,13 +128,16 @@ kubectl create secret generic "sendgrid" --from-env-file "/secrets/sendgrid.env"
 log "Setting up helm chart in cluster ${k8sname}"
 
 helm init --wait
+helm dependency update "${scriptdir}/helm"
 
 k8simageregistry="${KUBERNETES_IMAGE_REGISTRY:-ascoderu}"
 k8sdockertag="${KUBERNETES_DOCKER_TAG:-latest}"
+lokole_dns_name="${LOKOLE_DNS_NAME:-mailserver.lokole.ca}"
 
 while :; do
   helm install \
     --name "${helmname}" \
+    --set domain="${lokole_dns_name}" \
     --set version.imageRegistry="${k8simageregistry}" \
     --set version.dockerTag="${k8sdockertag}" \
     "${scriptdir}/helm"
@@ -142,7 +146,7 @@ while :; do
 done
 
 while :; do
-  ingressip="$(kubectl get service --selector io.kompose.service=nginx --output jsonpath={..ip})"
+  ingressip="$(kubectl get service --selector app=nginx-ingress,component=controller --output jsonpath={..ip})"
   if [[ -z "${ingressip}" ]]; then log "Waiting for ${k8sname} public IP"; sleep 30s; else break; fi
 done
 
@@ -150,17 +154,50 @@ cp ~/.kube/config /secrets/kube-config
 cp ~/.ssh/id_rsa.pub /secrets/kube-id_rsa.pub
 cp ~/.ssh/id_rsa /secrets/kube-id_rsa
 
+log "Setting up DNS for ${ingressip}"
+
+cloudflare_zone="$(get_dotenv '/secrets/cloudflare.env' 'LOKOLE_CLOUDFLARE_ZONE')"
+cloudflare_user="$(get_dotenv '/secrets/cloudflare.env' 'LOKOLE_CLOUDFLARE_USER')"
+cloudflare_key="$(get_dotenv '/secrets/cloudflare.env' 'LOKOLE_CLOUDFLARE_KEY')"
+cloudflare_dns_api="https://api.cloudflare.com/client/v4/zones/${cloudflare_zone}/dns_records"
+
+cloudflare_cname_id="$(curl -sX GET "${cloudflare_dns_api}?type=A&name=${lokole_dns_name}" \
+  -H "X-Auth-Email: ${cloudflare_user}" \
+  -H "X-Auth-Key: ${cloudflare_key}" \
+| jq -r '.result[0].id')"
+
+if [[ -n "${cloudflare_cname_id}" ]] && [[ ${cloudflare_cname_id} != "null" ]]; then
+  curl -sX PUT "${cloudflare_dns_api}/${cloudflare_cname_id}" \
+    -H "X-Auth-Email: ${cloudflare_user}" \
+    -H "X-Auth-Key: ${cloudflare_key}" \
+    -H "Content-Type: application/json" \
+    -d '{"type":"A","name":"'"${lokole_dns_name}"'","content":"'"${ingressip}"'","ttl":1,"proxied":false}'
+else
+  curl -sX POST "${cloudflare_dns_api}" \
+    -H "X-Auth-Email: ${cloudflare_user}" \
+    -H "X-Auth-Key: ${cloudflare_key}" \
+    -H "Content-Type: application/json" \
+    -d '{"type":"A","name":"'"${lokole_dns_name}"'","content":"'"${ingressip}"'","ttl":1,"proxied":false}'
+fi
+
 cat > /secrets/kubedeployment.env << EOF
 RESOURCE_GROUP=${KUBERNETES_RESOURCE_GROUP_NAME}
 HELM_NAME=${helmname}
 APP_IP=${ingressip}
+APP_DNS=${lokole_dns_name}
 EOF
+fi
 
-log "Backing up secrets"
+#
+# backup secrets
+#
 
 storage_account="$(get_dotenv '/secrets/azure.env' 'LOKOLE_EMAIL_SERVER_AZURE_BLOBS_NAME')"
 storage_key="$(get_dotenv '/secrets/azure.env' 'LOKOLE_EMAIL_SERVER_AZURE_BLOBS_KEY')"
-container_name="secrets-$(date +"%Y-%m-%d-%H-%M")"
+container_name="secrets"
+
+log "Backing up secrets to ${storage_account}/${container_name}"
+
 az storage container create --name "${container_name}" \
   --account-name="${storage_account}" --account-key="${storage_key}"
 az storage blob upload-batch --destination "${container_name}" --source "/secrets" \
--- opwen_email_server/constants/sendgrid.py
+++ opwen_email_server/constants/sendgrid.py
@@ -2,6 +2,6 @@ from typing_extensions import Final  # noqa: F401
 
 MAILBOX_URL = 'https://api.sendgrid.com/v3/user/webhooks/parse/settings'  # type: Final  # noqa: E501
 
-INBOX_URL = 'http://mailserver.lokole.ca/api/email/sendgrid/{}'  # type: Final
+INBOX_URL = 'https://mailserver.lokole.ca/api/email/sendgrid/{}'  # type: Final
 
 MX_RECORD = 'mx.sendgrid.net'  # type: Final
