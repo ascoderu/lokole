fb88c493db04f3baba63e115cf7043855c835bef
Clemens Wolff <clewolff@microsoft.com>
2019-01-03 21:48:47 -0500

    Add tests for Sendgrid's http client
    Resolves https://github.com/ascoderu/opwen-cloudserver/issues/120

--- tests/opwen_email_server/services/test_sendgrid.py
+++ tests/opwen_email_server/services/test_sendgrid.py
@@ -1,4 +1,6 @@
 from unittest import TestCase
+from unittest.mock import Mock
+from unittest.mock import patch
 
 from opwen_email_server.services.sendgrid import SendSendgridEmail
 
@@ -9,20 +11,15 @@ class SendgridEmailSenderTests(TestCase):
     sender = 'sendgridtests@lokole.ca'
 
     def test_sends_email(self):
-        send_email = SendSendgridEmail(key='')
-
-        success = send_email({
+        self.assertSendsEmail({
             'to': [self.recipient1],
             'from': self.sender,
             'subject': self.test_sends_email.__name__,
-            'message': 'simple email with <b>formatting</b>'})
-
-        self.assertTrue(success)
+            'message': 'simple email with <b>formatting</b>'
+        })
 
     def test_sends_email_with_attachments(self):
-        send_email = SendSendgridEmail(key='')
-
-        success = send_email({
+        self.assertSendsEmail({
             'to': [self.recipient1],
             'from': self.sender,
             'subject': self.test_sends_email_with_attachments.__name__,
@@ -31,17 +28,37 @@ class SendgridEmailSenderTests(TestCase):
                 {'filename': 'Some file.txt',
                  'content': b'first file'},
                 {'filename': 'Another file.txt',
-                 'content': b'second file'}]})
-
-        self.assertTrue(success)
+                 'content': b'second file'}]
+        })
 
     def test_sends_email_to_multiple_recipients(self):
-        send_email = SendSendgridEmail(key='')
-
-        success = send_email({
+        self.assertSendsEmail({
             'to': [self.recipient1, self.recipient2],
             'from': self.sender,
             'subject': self.test_sends_email_to_multiple_recipients.__name__,
-            'message': 'simple email with two recipients'})
+            'message': 'simple email with two recipients'
+        })
 
-        self.assertTrue(success)
+    def test_client_made_bad_request(self):
+        self.assertSendsEmail(
+            {'message': self.test_client_made_bad_request.__name__},
+            success=False,
+            status=400)
+
+    def assertSendsEmail(self, email: dict, success: bool = True, status: int = 200):
+        with patch('urllib.request.build_opener') as mock_build_opener:
+            self.given_response_status(mock_build_opener, status)
+
+            send_email = SendSendgridEmail(key='fake')
+
+            send_success = send_email(email)
+
+            self.assertTrue(send_success if success else not send_success)
+
+    @classmethod
+    def given_response_status(cls, mock_build_opener, status):
+        mock_opener = Mock()
+        mock_response = Mock()
+        mock_build_opener.return_value = mock_opener
+        mock_opener.open.return_value = mock_response
+        mock_response.getcode.return_value = status
