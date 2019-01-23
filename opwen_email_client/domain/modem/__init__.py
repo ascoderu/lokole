a83ef4ac755c97f1e70125c081ae6a1f02500b10
Clemens Wolff <clemens@justamouse.com>
2019-01-23 14:03:28 -0500

    Make check_call argument a string (#123)
    According to the docs [1]:
    
    ```
    If shell is True, it is recommended to pass args as a string rather than
    as a sequence.
    ```
    
    [1] https://docs.python.org/3/library/subprocess.html#subprocess.Popen

--- opwen_email_client/domain/modem/__init__.py
+++ opwen_email_client/domain/modem/__init__.py
@@ -3,10 +3,6 @@ from subprocess import check_call  # nosec
 from subprocess import check_output  # nosec
 
 
-def _check_process_output(command):
-    return check_output(command, shell=True)  # nosec
-
-
 def _find_device(stdout: bytes, device_id: str):
     for line in stdout.splitlines():
         line = line.decode('utf-8')
@@ -18,15 +14,15 @@ def _find_device(stdout: bytes, device_id: str):
 
 
 def modem_is_plugged(modem=None):
-    result = _check_process_output(['/usr/bin/lsusb'])
+    result = check_output('/usr/bin/lsusb', shell=True)  # nosec
     return _find_device(result, modem.uid if modem else '12d1:')
 
 
 def modem_is_setup(target_mode: str):
-    result = _check_process_output(['/usr/bin/lsusb'])
+    result = check_output('/usr/bin/lsusb', shell=True)  # nosec
     return _find_device(result, '12d1:{}'.format(target_mode))
 
 
 def setup_modem(config: Path):
-    check_call(['/usr/sbin/usb_modeswitch',
-                '--config-file', str(config.absolute())], shell=True)  # nosec
+    check_call('/usr/sbin/usb_modeswitch --config-file="{}"'
+               .format(config.absolute()), shell=True)  # nosec
