e244a618688cc12c402bdf0caffb3205b9fa6639
Clemens Wolff <clemens@justamouse.com>
2019-02-02 18:36:38 -0500

    Make wvdial more generic (#127)

--- opwen_email_client/domain/sim/hologram.py
+++ opwen_email_client/domain/sim/hologram.py
@@ -1,11 +1,13 @@
 wvdial = (
     '[Dialer Defaults]\n'
     'Init1 = ATZ\n'
-    'Init3 = AT+CGDCONT=1,"IP","hologram"\n'
+    'Init2 = ATQ0\n'
+    'Init3 = AT+CGDCONT=1,"IP","apn.konekt.io"\n'
     'Phone = *99***1#\n'
     'Stupid Mode = 1\n'
-    'Username = " "\n'
-    'Password = " "\n'
-    'New PPPD = yes\n'
+    'Username = { }\n'
+    'Password = { }\n'
+    'Modem Type = Analog Modem\n'
     'Modem = /dev/ttyUSB0\n'
+    'IDSN = 0\n'
 )
