07ae51505144c4bb11413ac0e1dc384d61a77f09
Clemens Wolff <clemens@justamouse.com>
2019-02-27 21:21:44 -0500

    Switch to Azurite as the local storage layer (#139)
    * Update client package test file to new tar format
    
    * Use Azurite as the storage when running locally

--- opwen_email_server/integration/azure.py
+++ opwen_email_server/integration/azure.py
@@ -20,6 +20,8 @@ def get_auth() -> AzureAuth:
         storage=AzureTextStorage(
             account=config.TABLES_ACCOUNT,
             key=config.TABLES_KEY,
+            host=config.TABLES_HOST,
+            secure=config.TABLES_SECURE,
             container=constants.TABLE_AUTH,
             provider=config.STORAGE_PROVIDER))
 
@@ -30,6 +32,8 @@ def get_client_storage() -> AzureObjectsStorage:
         file_storage=AzureFileStorage(
             account=config.CLIENT_STORAGE_ACCOUNT,
             key=config.CLIENT_STORAGE_KEY,
+            host=config.CLIENT_STORAGE_HOST,
+            secure=config.CLIENT_STORAGE_SECURE,
             container=constants.CONTAINER_CLIENT_PACKAGES,
             provider=config.STORAGE_PROVIDER))
 
@@ -39,6 +43,8 @@ def get_raw_email_storage() -> AzureTextStorage:
     return AzureTextStorage(
         account=config.BLOBS_ACCOUNT,
         key=config.BLOBS_KEY,
+        host=config.BLOBS_HOST,
+        secure=config.BLOBS_SECURE,
         container=constants.CONTAINER_SENDGRID_MIME,
         provider=config.STORAGE_PROVIDER)
 
@@ -68,6 +74,8 @@ def get_email_storage() -> AzureObjectStorage:
     return AzureObjectStorage(
         account=config.BLOBS_ACCOUNT,
         key=config.BLOBS_KEY,
+        host=config.BLOBS_HOST,
+        secure=config.BLOBS_SECURE,
         container=constants.CONTAINER_EMAILS,
         provider=config.STORAGE_PROVIDER)
 
@@ -78,5 +86,7 @@ def get_pending_storage(domain: str) -> AzureTextStorage:
     return AzureTextStorage(
         account=config.TABLES_ACCOUNT,
         key=config.TABLES_KEY,
+        host=config.TABLES_HOST,
+        secure=config.TABLES_SECURE,
         container=container,
         provider=config.STORAGE_PROVIDER)
