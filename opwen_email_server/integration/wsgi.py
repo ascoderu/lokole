c22a680e78b4ae4a5d9ae71f757045ee0ad7651f
Clemens Wolff <clewolff@microsoft.com>
2019-04-28 22:56:03 -0400

    Hard-code flask server

--- opwen_email_server/integration/wsgi.py
+++ opwen_email_server/integration/wsgi.py
@@ -2,19 +2,15 @@
 
 from connexion import App
 
-from opwen_email_server.utils.imports import can_import
-
-_servers = list(filter(can_import, ('tornado', 'gevent', 'flask')))
 _hosts = ['127.0.0.1', '0.0.0.0']  # nosec
 
-_server = _servers[0]
 _host = _hosts[0]
 _port = 8080
 _ui = False
 
 
-def build_app(apis, host=_host, port=_port, server=_server, ui=_ui):
-    app = App(__name__, host=host, port=port, server=server,
+def build_app(apis, host=_host, port=_port, ui=_ui):
+    app = App(__name__, host=host, port=port, server='flask',
               options={'swagger_ui': ui})
 
     for api in apis:
@@ -30,7 +26,6 @@ def _cli():
     parser = ArgumentParser()
     parser.add_argument('--host', choices=_hosts, default=_host)
     parser.add_argument('--port', type=int, default=_port)
-    parser.add_argument('--server', choices=_servers, default=_server)
     parser.add_argument('--ui', action='store_true', default=_ui)
     parser.add_argument('apis', nargs='+', type=FileType('r'))
     args = parser.parse_args()
@@ -40,7 +35,7 @@ def _cli():
         apis.append(fobj.name)
         fobj.close()
 
-    app = build_app(apis, args.host, args.port, args.server, args.ui)
+    app = build_app(apis, args.host, args.port, args.ui)
 
     app.run()
 
