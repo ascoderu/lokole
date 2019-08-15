4495f8a90490ba320036268cedee4a376a407391
Clemens Wolff <clemens@ascoderu.ca>
2019-08-15 16:22:21 -0400

    Format all Javascript with prettier (#203)

--- opwen_email_client/webapp/static/js/_base_email.js
+++ opwen_email_client/webapp/static/js/_base_email.js
@@ -1,33 +1,31 @@
 (function($) {
-$(document).ready(function() {
+  $(document).ready(function() {
+    (function activateLazyload() {
+      $(".email-body img").lazyload();
+    })();
 
-(function activateLazyload() {
-  $(".email-body img").lazyload();
-}());
-
-(function markEmailsAsReadOnOpen() {
-  $(".panel-info").click(function() {
-    var $el = $(this);
-    if ($el.hasClass("panel-info")) {
-      var email_id = $el.data("email_id");
-      if (email_id) {
-        $.ajax({
-          url: "/email/read/" + email_id,
-          success: function() {
-            $el.removeClass("panel-info").addClass("panel-default");
+    (function markEmailsAsReadOnOpen() {
+      $(".panel-info").click(function() {
+        var $el = $(this);
+        if ($el.hasClass("panel-info")) {
+          var email_id = $el.data("email_id");
+          if (email_id) {
+            $.ajax({
+              url: "/email/read/" + email_id,
+              success: function() {
+                $el.removeClass("panel-info").addClass("panel-default");
+              }
+            });
           }
-        });
-      }
-    }
-  });
-}());
+        }
+      });
+    })();
 
-(function printEmailOnPrintButtonClick() {
-  $(".print-trigger").click(function() {
-    var $printRoot = $(this).closest(".print-root");
-    $printRoot.printThis();
+    (function printEmailOnPrintButtonClick() {
+      $(".print-trigger").click(function() {
+        var $printRoot = $(this).closest(".print-root");
+        $printRoot.printThis();
+      });
+    })();
   });
-}());
-
-});
 })(window.jQuery);
--- opwen_email_client/webapp/static/js/settings.js
+++ opwen_email_client/webapp/static/js/settings.js
@@ -1,103 +1,108 @@
 (function($) {
-var API_BASE = '/api/mkwvconf';
+  var API_BASE = "/api/mkwvconf";
 
-var $modal = $('#wvdial-modal');
-var $countrySelect = $('#wvdial-country');
-var $providerSelect = $('#wvdial-provider');
-var $apnSelect = $('#wvdial-apn');
-var $submitButton = $('#generate-wvdial-config');
+  var $modal = $("#wvdial-modal");
+  var $countrySelect = $("#wvdial-country");
+  var $providerSelect = $("#wvdial-provider");
+  var $apnSelect = $("#wvdial-apn");
+  var $submitButton = $("#generate-wvdial-config");
 
-function updateModalState() {
-  var country = $countrySelect.val();
-  var provider = $providerSelect.val();
-  var apn = $apnSelect.val();
+  function updateModalState() {
+    var country = $countrySelect.val();
+    var provider = $providerSelect.val();
+    var apn = $apnSelect.val();
 
-  var providerDisabled = !country;
-  var apnDisabled = !country || !provider;
-  var submitDisabled = !country || !provider || !apn;
+    var providerDisabled = !country;
+    var apnDisabled = !country || !provider;
+    var submitDisabled = !country || !provider || !apn;
 
-  $providerSelect.prop('disabled', providerDisabled);
-  $apnSelect.prop('disabled', apnDisabled);
-  $submitButton.prop('disabled', submitDisabled);
+    $providerSelect.prop("disabled", providerDisabled);
+    $apnSelect.prop("disabled", apnDisabled);
+    $submitButton.prop("disabled", submitDisabled);
 
-  if (providerDisabled) $providerSelect.empty();
-  if (apnDisabled) $apnSelect.empty();
-}
-
-function autoSelectOption($select) {
-  var options = $select.find('option');
-
-  var hasDummyOption = options.length > 0 && options[0].value === '';
-  var hasOnlyOneRealOption = hasDummyOption && options.length === 2;
-
-  if (hasOnlyOneRealOption) {
-    $select.val(options[1].value).trigger('change');
+    if (providerDisabled) $providerSelect.empty();
+    if (apnDisabled) $apnSelect.empty();
   }
-}
 
-function populateSelect($select, items) {
-  $('<option/>').text('').appendTo($select);
+  function autoSelectOption($select) {
+    var options = $select.find("option");
 
-  items.forEach(function(item) {
-    $('<option/>').text(item).appendTo($select);
+    var hasDummyOption = options.length > 0 && options[0].value === "";
+    var hasOnlyOneRealOption = hasDummyOption && options.length === 2;
+
+    if (hasOnlyOneRealOption) {
+      $select.val(options[1].value).trigger("change");
+    }
+  }
+
+  function populateSelect($select, items) {
+    $("<option/>")
+      .text("")
+      .appendTo($select);
+
+    items.forEach(function(item) {
+      $("<option/>")
+        .text(item)
+        .appendTo($select);
+    });
+
+    autoSelectOption($select);
+  }
+
+  $modal.on("show.bs.modal", function() {
+    $.getJSON(API_BASE + "/", function(response) {
+      populateSelect($countrySelect, response.countries);
+    });
+
+    updateModalState();
   });
 
-  autoSelectOption($select);
-}
+  $countrySelect.on("change", function() {
+    var country = $countrySelect.val();
 
-$modal.on('show.bs.modal', function() {
-  $.getJSON(API_BASE + '/', function(response) {
-    populateSelect($countrySelect, response.countries);
+    if (country) {
+      $providerSelect.empty();
+      $apnSelect.empty();
+
+      $.getJSON(API_BASE + "/" + country, function(response) {
+        populateSelect($providerSelect, response.providers);
+      });
+    }
+
+    updateModalState();
   });
 
-  updateModalState();
-});
+  $providerSelect.on("change", function() {
+    var provider = $providerSelect.val();
+    var country = $countrySelect.val();
 
-$countrySelect.on('change', function() {
-  var country = $countrySelect.val();
+    if (country && provider) {
+      $apnSelect.empty();
 
-  if (country) {
-    $providerSelect.empty();
-    $apnSelect.empty();
+      $.getJSON(API_BASE + "/" + country + "/" + provider, function(response) {
+        populateSelect($apnSelect, response.apns);
+      });
+    }
 
-    $.getJSON(API_BASE + '/' + country, function(response) {
-      populateSelect($providerSelect, response.providers);
-    });
-  }
+    updateModalState();
+  });
 
-  updateModalState();
-});
+  $apnSelect.on("change", function() {
+    updateModalState();
+  });
 
-$providerSelect.on('change', function() {
-  var provider = $providerSelect.val();
-  var country = $countrySelect.val();
+  $submitButton.on("click", function() {
+    var country = $countrySelect.val();
+    var provider = $providerSelect.val();
+    var apn = $apnSelect.val();
 
-  if (country && provider) {
-    $apnSelect.empty();
-
-    $.getJSON(API_BASE + '/' + country + '/' + provider, function(response) {
-      populateSelect($apnSelect, response.apns);
-    });
-  }
-
-  updateModalState();
-});
-
-$apnSelect.on('change', function() {
-  updateModalState();
-});
-
-$submitButton.on('click', function() {
-  var country = $countrySelect.val();
-  var provider = $providerSelect.val();
-  var apn = $apnSelect.val();
-
-  if (country && provider && apn) {
-    $.getJSON(API_BASE + '/' + country + '/' + provider + '/' + apn, function(response) {
-      $('#wvdial').val(response.config);
-      $('#sim_type').val('wvdial');
-      $modal.modal('hide');
-    });
-  }
-});
+    if (country && provider && apn) {
+      var endpoint = API_BASE + "/" + country + "/" + provider + "/" + apn;
+      $.getJSON(endpoint, function(response) {
+        $("#wvdial").val(response.config);
+        $("#sim_type").val("wvdial");
+        $modal.modal("hide");
+      });
+    }
+  });
 })(window.jQuery);
