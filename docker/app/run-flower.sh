18aa771c225ea5b3aaca3e42b7158ae813445baa
Clemens Wolff <clewolff@microsoft.com>
2019-11-24 19:33:52 -0500

    Move flower from production to devtool

--- docker/app/run-flower.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-"${PY_ENV}/bin/flower" \
-  --app="opwen_email_server.integration.celery" \
-  --port="${PORT}" \
-  --url_prefix="flower" \
-  --basic_auth="${FLOWER_USERNAME}:${FLOWER_PASSWORD}"
