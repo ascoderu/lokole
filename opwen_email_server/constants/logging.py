6bdf9a3324b9af3da2f0c41073147a4b5c645af6
Clemens Wolff <clemens@ascoderu.ca>
2019-12-21 21:03:43 -0500

    Increase stderr logging (#263)
    * Fix local appinsights logging
    
    * Increase stderr log level but not appinsights
    
    * Simplify logger name injection

--- opwen_email_server/constants/logging.py
+++ opwen_email_server/constants/logging.py
@@ -1,4 +1,4 @@
 from typing_extensions import Final  # noqa: F401
 
-STDERR = '%(asctime)s\t%(levelname)s\t%(message)s'  # type: Final
-SEPARATOR = '|'  # type: Final
+APPINSIGHTS = '%(name)s\t%(message)s'  # type: Final
+STDERR = '%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s'  # type: Final
