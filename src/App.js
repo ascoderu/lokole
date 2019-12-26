996194f3e8189618b404684a5a519409b6f4a986
Clemens Wolff <clewolff@microsoft.com>
2019-12-26 00:39:36 -0500

    Add antd component library

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
