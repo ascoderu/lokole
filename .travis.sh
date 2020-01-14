4470034ad9efb335260a4aff0ca706f511536448
Clemens Wolff <clewolff@microsoft.com>
2020-01-14 15:46:50 -0500

    Extract Travis steps to script

---
+++ .travis.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+case "$1" in
+  before_install)
+    sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose version
+    ;;
+
+  script)
+    if [[ "$TRAVIS_EVENT_TYPE" = "cron" ]]; then
+      make renew-cert
+    else
+      echo "Skipping cert renewal for on-demand build"
+    fi
+    if [[ "$TEST_MODE" = "live" ]]; then
+      export REGISTRATION_CREDENTIALS="$GITHUB_AUTH_USERNAME:$GITHUB_AUTH_TOKEN"
+      export LOKOLE_QUEUE_BROKER_SCHEME="azureservicebus"
+    else
+      export REGISTRATION_CREDENTIALS="admin:password"
+      export LOKOLE_QUEUE_BROKER_SCHEME="amqp"
+    fi
+    if [[ "$TRAVIS_PULL_REQUEST" = "false" ]] && [[ "$TEST_MODE" = "live" ]]; then
+      echo "Skipping live service test for branch build"
+    elif [[ "$TRAVIS_PULL_REQUEST_SLUG" != "ascoderu/opwen-cloudserver" ]] && [[ "$TEST_MODE" = "live" ]]; then
+      echo "Skipping live service test for fork build"
+    else
+      export BUILD_TARGET=runtime
+      make build verify-build
+      make start
+      make integration-tests
+    fi
+    ;;
+
+  after_script)
+    make stop
+    ;;
+
+  after_success)
+    if [[ "$TEST_MODE" = "local" ]]; then
+      bash <(curl -s https://codecov.io/bash)
+    fi
+    ;;
+
+  after_failure)
+    ALL=true make logs
+    ;;
+
+  before_deploy)
+    echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+    ;;
+
+  deploy)
+    if [[ "$TEST_MODE" = "live" ]]; then
+      DOCKER_TAG="$TRAVIS_TAG" make release deploy
+    fi
+    ;;
+
+  *)
+    echo "Unrecognized command: $1" >&2
+    exit 1
+    ;;
+esac
