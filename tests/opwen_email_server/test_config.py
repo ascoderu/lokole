6b251171b62ae7938b3d6524e44abd81c6be01e2
Clemens Wolff <clewolff@microsoft.com>
2020-02-01 23:34:29 -0500

    Move container names to config

--- tests/opwen_email_server/test_config.py
+++ tests/opwen_email_server/test_config.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from importlib import reload
 from os import environ
+from re import match
 from unittest import TestCase
 
 from opwen_email_server import config
@@ -33,6 +34,23 @@ class ConfigTests(TestCase):
         }
         with setenvs(envs):
             self.assertEqual(config.QUEUE_BROKER, 'azureservicebus://us%2Fer:pass@host')
+
+    def test_container_names_are_valid(self):
+        acceptable_container_name = '^[a-z0-9][a-z0-9-]{2,62}$'
+
+        for constant, value in get_constants(config):
+            if constant.startswith('CONTAINER_') and not match(acceptable_container_name, value):
+                self.fail(f'config {constant} is invalid: {value}, ' f'should be {acceptable_container_name}')
+
+
+def get_constants(container):
+    for variable_name in dir(container):
+        if variable_name.upper() != variable_name:
+            continue
+        value = getattr(container, variable_name)
+        if not isinstance(value, str):
+            continue
+        yield variable_name, value
 
 
 @contextmanager
