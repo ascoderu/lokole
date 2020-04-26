9b7f5493dc89bff0e75f15bb4ceea16856d34bcb
Clemens Wolff <clewolff@microsoft.com>
2020-04-26 11:46:36 -0400

    Make nginx config compatible with system setup

--- docker/nginx/run-nginx.sh
+++ docker/nginx/run-nginx.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 mo < /app/nginx.conf.template > /app/nginx.conf
+mo < /app/server.conf.template > /etc/nginx/sites-enabled/server.conf
 
 nginx -c "/app/nginx.conf" -p "${PWD}" -g "daemon off;"
