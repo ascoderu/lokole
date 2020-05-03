14f0f8bea288f8b6510d7337a5300766891ad55d
Clemens Wolff <clewolff@microsoft.com>
2020-05-03 00:28:36 -0400

    Visualize test delays

--- docker/integtest/tests.sh
+++ docker/integtest/tests.sh
@@ -2,11 +2,13 @@
 set -eo pipefail
 
 scriptdir="$(dirname "$0")"
+# shellcheck disable=SC1090
+. "${scriptdir}/utils.sh"
 
 "${scriptdir}/0-wait-for-services.sh"
 "${scriptdir}/1-register-client.sh"
-"${scriptdir}/2-client-uploads-emails.sh" && sleep "${TEST_STEP_DELAY}"
-"${scriptdir}/3-receive-email-for-client.sh" && sleep "${TEST_STEP_DELAY}"
+"${scriptdir}/2-client-uploads-emails.sh" && wait_seconds "${TEST_STEP_DELAY}"
+"${scriptdir}/3-receive-email-for-client.sh" && wait_seconds "${TEST_STEP_DELAY}"
 "${scriptdir}/4-client-downloads-emails.sh"
 "${scriptdir}/5-assert-on-results.sh"
 
