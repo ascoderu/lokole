246b210507119e4d958bb403b55a5af89e017573
Clemens Wolff <clewolff@microsoft.com>
2020-05-03 00:10:51 -0400

    Increase integration test delay for live tests

--- docker/integtest/tests.sh
+++ docker/integtest/tests.sh
@@ -5,8 +5,8 @@ scriptdir="$(dirname "$0")"
 
 "${scriptdir}/0-wait-for-services.sh"
 "${scriptdir}/1-register-client.sh"
-"${scriptdir}/2-client-uploads-emails.sh" && sleep 10s
-"${scriptdir}/3-receive-email-for-client.sh" && sleep 10s
+"${scriptdir}/2-client-uploads-emails.sh" && sleep "${TEST_STEP_DELAY}"
+"${scriptdir}/3-receive-email-for-client.sh" && sleep "${TEST_STEP_DELAY}"
 "${scriptdir}/4-client-downloads-emails.sh"
 "${scriptdir}/5-assert-on-results.sh"
 
