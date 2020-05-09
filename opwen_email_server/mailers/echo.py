6aa2b3c5f4005ceb0bbd1a9f0fa616b3dd7cc6ad
Clemens Wolff <clewolff@microsoft.com>
2020-05-08 20:36:43 -0400

    Ensure sent_at is set in echo email

--- opwen_email_server/mailers/echo.py
+++ opwen_email_server/mailers/echo.py
@@ -1,10 +1,17 @@
+from datetime import datetime
+from typing import Callable
+
 from opwen_email_server.utils.log import LogMixin
 
 ECHO_ADDRESS = 'echo@bot.lokole.ca'
 
 
 class EchoEmailFormatter(LogMixin):
+    def __init__(self, now: Callable[[], datetime] = datetime.utcnow):
+        self._now = now
+
     def __call__(self, email: dict) -> dict:
         email['to'] = [email['from']]
         email['from'] = ECHO_ADDRESS
+        email['sent_at'] = self._now().strftime('%Y-%m-%d %H:%M')
         return email
