2e84d152a2a059e56bdc8a58e69811e09b0fef4d
Clemens Wolff <clemens@ascoderu.ca>
2020-05-08 20:07:54 -0400

    Add more logging to webmail (#437)

--- opwen_email_server/integration/webapp.py
+++ opwen_email_server/integration/webapp.py
@@ -27,6 +27,7 @@ from opwen_email_server.utils.collections import chunks
 from opwen_email_server.utils.email_parser import ensure_has_sent_at
 from opwen_email_server.utils.email_parser import get_domain
 from opwen_email_server.utils.email_parser import get_recipients
+from opwen_email_server.utils.log import LogMixin
 
 
 class AzureRole:
@@ -124,7 +125,7 @@ class AzureUserStore(UserStore, UserReadStore, UserWriteStore):
         return f'{get_domain(email)}/{email}'
 
 
-class AzureEmailStore(EmailStore):
+class AzureEmailStore(EmailStore, LogMixin):
     def __init__(self, email_storage: AzureObjectStorage, mailbox_storage: AzureTextStorage,
                  pending_storage: AzureTextStorage, send_email: Callable[[str], None]):
         super().__init__(restricted=None)
@@ -153,6 +154,7 @@ class AzureEmailStore(EmailStore):
         try:
             email = self._email_storage.fetch_object(uid)
         except ObjectDoesNotExistError:
+            self.log_warning('Email at %s does not exist', uid)
             return None
         else:
             email['read'] = True
