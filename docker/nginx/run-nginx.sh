9f8a69acf9aa3921b94e506d22e056b8df480506
Clemens Wolff <clewolff@microsoft.com>
2020-05-11 15:40:09 -0400

    Make docker CMD runner scripts consistent

--- docker/nginx/run-nginx.sh
+++ docker/nginx/run-nginx.sh
@@ -3,4 +3,4 @@
 mo < /app/nginx.conf.template > /app/nginx.conf
 mo < /app/server.conf.template > /etc/nginx/sites-enabled/server.conf
 
-nginx -c "/app/nginx.conf" -p "${PWD}" -g "daemon off;"
+exec nginx -c "/app/nginx.conf" -p "${PWD}" -g "daemon off;"
