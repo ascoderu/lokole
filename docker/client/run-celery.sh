79a8a9d7fa4e8da5dc780a188bf6f671629216b4
Clemens Wolff <clemens@ascoderu.ca>
2020-05-11 18:16:53 -0400

    Cleanup unused images after deployment (#451)
    * Cleanup unused images after deployment
    
    * Make docker CMD runner scripts consistent
    
    * Make mustache template file extensions explicit

--- docker/client/run-celery.sh
+++ docker/client/run-celery.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 exec celery \
-  --app=opwen_email_client.webapp.tasks \
+  --app="opwen_email_client.webapp.tasks" \
   worker \
   --loglevel="${LOKOLE_LOG_LEVEL}" \
   --concurrency="${QUEUE_WORKERS}"
