27a2e4439e48472e0281f16058bc2975b23075bd
Adam Sclafani <adam.sclafani15@gmail.com>
2020-06-06 18:43:05 -1000

    switched out html version for pdf

--- opwen_email_server/mailers/wikipedia.py
+++ opwen_email_server/mailers/wikipedia.py
@@ -1,6 +1,8 @@
+from base64 import b64encode
 from datetime import datetime
 from typing import Callable
 
+from requests import get
 from wikipedia import languages
 from wikipedia import page
 from wikipedia import set_lang
@@ -15,6 +17,10 @@ WIKIPEDIA_ADDRESS = 'wikipedia@bot.lokole.ca'
 class WikipediaEmailFormatter(LogMixin):
     def __init__(self, now: Callable[[], datetime] = datetime.utcnow):
         self._now = now
+
+    def _get_download_link(self, url: str) -> str:
+        split_url = url.split('/')
+        return 'https://' + split_url[2] + '/api/rest_v1/page/pdf/' + split_url[-1]
 
     def __call__(self, email: dict) -> dict:
         language = email['subject']
@@ -33,7 +39,12 @@ class WikipediaEmailFormatter(LogMixin):
             body = 'No results found for: {}'.format(email['body'])
         else:
             subject = wiki_page.title
-            body = wiki_page.html()
+            body = ''
+            download_link = self._get_download_link(wiki_page.url)
+            pdf_file = get(download_link)
+            email['attachments'] = [{'filename': subject + '.pdf',
+                                     'content': b64encode(pdf_file.content),
+                                     }]
 
         email['to'] = [email['from']]
         email['from'] = WIKIPEDIA_ADDRESS
