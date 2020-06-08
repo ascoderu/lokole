d6a189cdce18d9c9fed105173a0a06d058a825cb
Adam Sclafani <adam.sclafani15@gmail.com>
2020-06-07 14:15:13 -1000

    integration test changes

--- docker/integtest/6-receive-service-email.sh
+++ docker/integtest/6-receive-service-email.sh
@@ -8,21 +8,18 @@ mkdir -p "${out_dir}"
 # shellcheck disable=SC1090
 . "${scriptdir}/utils.sh"
 
-email_to_receive="${in_dir}/echo-service-email.mime"
-
+echo_email_to_receive="${in_dir}/echo-service-email.mime"
+wikipedia_email_to_receive="${in_dir}/wikipedia-service-email.mime"
 
 #receive an email directed at the service endpoint
 http --check-status -f POST \
   "http://nginx:8888/api/email/sendgrid/service" \
   "dkim={@sendgrid.com : pass}" \
   "SPF=pass" \
-  "email=@${email_to_receive}"
+  "email=@${echo_email_to_receive}"
 
- email_to_receive="${in_dir}/wikipedia-service-email.mime"
-
-
- http --check-status -f POST \
-   "http://nginx:8888/api/email/sendgrid/service" \
-   "dkim={@sendgrid.com : pass}" \
-   "SPF=pass" \
-   "email=@${email_to_receive}"
+http --check-status -f POST \
+  "http://nginx:8888/api/email/sendgrid/service" \
+  "dkim={@sendgrid.com : pass}" \
+  "SPF=pass" \
+  "email=@${wikipedia_email_to_receive}"
