e831f774c9a1256505cc3193d1bb1b7dd2c9cf61
Shaun B <me@shaunb.ca>
2020-10-28 10:27:43 -0700

    Add Cloudflare configuration

--- docker/setup/terraform/01_provider.tf
+++ docker/setup/terraform/01_provider.tf
@@ -1,8 +1,16 @@
+variable "client_secret" {
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you're using version 1.x, the "features" block is not allowed.
   version = "=2.23.0"
+
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.SP_APPID
+  client_secret   = var.SP_PASSWORD
+  tenant_id       = var.SP_TENANT
 
   features {}
 }
