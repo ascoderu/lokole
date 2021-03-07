161ed474fdcdd19dd0488a5e1d034ffe12490756
Clemens Wolff <clewolff@microsoft.com>
2021-03-07 11:10:21 -0500

    Extract DNS setup script

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -188,31 +188,9 @@ if [[ "${DEPLOY_COMPUTE}" != "no" ]]; then
   cp ~/.ssh/id_rsa.pub /secrets/kube-id_rsa.pub
   cp ~/.ssh/id_rsa /secrets/kube-id_rsa
 
-  log "Setting up DNS for ${ingressip}"
-
-  cloudflare_zone="$(get_dotenv '/secrets/cloudflare.env' 'LOKOLE_CLOUDFLARE_ZONE')"
-  cloudflare_user="$(get_dotenv '/secrets/cloudflare.env' 'LOKOLE_CLOUDFLARE_USER')"
-  cloudflare_key="$(get_dotenv '/secrets/cloudflare.env' 'LOKOLE_CLOUDFLARE_KEY')"
-  cloudflare_dns_api="https://api.cloudflare.com/client/v4/zones/${cloudflare_zone}/dns_records"
-
-  cloudflare_cname_id="$(curl -sX GET "${cloudflare_dns_api}?type=A&name=${lokole_dns_name}" \
-    -H "X-Auth-Email: ${cloudflare_user}" \
-    -H "X-Auth-Key: ${cloudflare_key}" |
-    jq -r '.result[0].id')"
-
-  if [[ -n "${cloudflare_cname_id}" ]] && [[ ${cloudflare_cname_id} != "null" ]]; then
-    curl -sX PUT "${cloudflare_dns_api}/${cloudflare_cname_id}" \
-      -H "X-Auth-Email: ${cloudflare_user}" \
-      -H "X-Auth-Key: ${cloudflare_key}" \
-      -H "Content-Type: application/json" \
-      -d '{"type":"A","name":"'"${lokole_dns_name}"'","content":"'"${ingressip}"'","ttl":1,"proxied":false}'
-  else
-    curl -sX POST "${cloudflare_dns_api}" \
-      -H "X-Auth-Email: ${cloudflare_user}" \
-      -H "X-Auth-Key: ${cloudflare_key}" \
-      -H "Content-Type: application/json" \
-      -d '{"type":"A","name":"'"${lokole_dns_name}"'","content":"'"${ingressip}"'","ttl":1,"proxied":false}'
-  fi
+  LOKOLE_SERVER_IP="${ingressip}" \
+  LOKOLE_DNS_NAME="${lokole_dns_name}" \
+  ./setup-dns.sh
 
   ./renew-cert.sh
 
