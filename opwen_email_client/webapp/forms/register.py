753c0e48a4ff64c10ffd7ea41b0e9bab9f37c497
Adam Sclafani <37944482+adamsclafani@users.noreply.github.com>
2021-05-28 10:30:09 -1000

    Improvements/Bug fixes (#548)
    * status code fix, bug fixes on the client_register action
    
    * simplification, remove unecessary path setup function
    
    * bring up registration page upon admin login if needed
    
    * ci feedback

--- opwen_email_client/webapp/forms/register.py
+++ opwen_email_client/webapp/forms/register.py
@@ -1,6 +1,5 @@
 from os import getenv
 from pathlib import Path
-from shutil import chown
 
 from flask_wtf import FlaskForm
 from requests import post
@@ -21,8 +20,8 @@ class RegisterForm(FlaskForm):
     submit = SubmitField()
 
     def register_client(self):
-        path = self._setup_path()
-        self._setup_client(path)
+        path = (Path(getenv('OPWEN_STATE_DIRECTORY', 'lokole/state')) / 'settings.env')
+        self._setup_client(str(path))
 
     def _setup_client(self, path):
         name = self.client_name.data.strip()
@@ -35,20 +34,6 @@ class RegisterForm(FlaskForm):
         response = post(client_create_url,
                         json={'domain': client_domain},
                         headers={'Authorization': 'Bearer {}'.format(token)})
-        if response.status_code != 200:
+        if response.status_code != 201:
             raise ValidationError(i8n.FAILED_REGISTRATION)
         register.delay(name, token, path)
-
-    def _setup_path(self):
-        home = Path.home()
-        user = home.parts[-1]
-        path = (Path(getenv('OPWEN_STATE_DIRECTORY', 'lokole/state')) / 'settings.env').absolute()
-        parent = path.parent
-        parent.mkdir(parents=True, exist_ok=True)
-        is_in_home = parent.parts[:3] == home.parts
-        if is_in_home:
-            home_parts = parent.parts[3:]
-            for part in home_parts:
-                home /= part
-                chown(str(home), user, user)
-        return str(path)
